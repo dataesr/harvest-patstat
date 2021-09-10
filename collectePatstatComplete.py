575bc2e3cc1e1cb3579cbc00ecceb0ba162e7940
juliaGrandhay <julia.grandhay@gmail.com>
2021-09-10 18:07:38 +0200

    Programme réorganisé sous forme de fonctions Reste à faire le dézippage des fichiers et la sécurisation de l'éxécution

--- collectePatstatComplete.py
+++ collectePatstatComplete.py
@@ -2,32 +2,48 @@ from bs4 import BeautifulSoup
 import requests
 import config
 import re
-import zipfile
-
-# ouverture de la session de requête
-s = requests.session()
-
-# identification sur Patstat
-s.get(f'https://publication.epo.org/raw-data/authentication?login={config.USER}&pwd={config.PWD}&action=1&format=1')
-
-# requête pour accéder au XML de la page contenant les liens vers tous les fichiers zippés
-r = s.get("https://publication.epo.org/raw-data/products/86/editions/6891/files")
-
-# "parse" le XML pour extraire la liste des URLs
-soup = BeautifulSoup(r.text, "lxml")
-listUrl = [elt.text for elt in soup.findAll("url")]
-
-listNoms = [x for x in map(lambda a: re.sub("https://publication.epo.org/raw-data/products/86/editions/6891/files/",
-                                            "",
-                                            a),
-                           listUrl)]
 
 
 def appel_patstat(n):
     return s.get(n)
 
 
-for i in range(len(listUrl)):
-    req = appel_patstat(listUrl[i])
-    with open(listNoms[i], "wb") as code:
-        code.write(req.content)
+def connexion_api(adresse):
+    session = requests.session()  # ouverture de la session de requête
+    # identification sur Patstat
+    session.get(adresse)
+    return session
+
+
+def extraction_url(contenu):
+    soup = BeautifulSoup(contenu.text, "lxml")
+    listurl = [elt.text for elt in soup.findAll("url")]
+    return listurl
+
+
+def liste_noms(listurl):
+    listnoms = list(map(lambda a: re.sub("https://publication.epo.org/raw-data/products/86/editions/6891/files/",
+                                         "",
+                                         a),
+                        listurl))
+    return listnoms
+
+
+def telechargement_ecriture(listurl, listnoms):
+    for i in range(len(listurl)):
+        req = appel_patstat(listurl[i])
+        with open(listnoms[i], "wb") as code:
+            code.write(req.content)
+
+
+urlId = f"https://publication.epo.org/raw-data/authentication?login={config.USER}&pwd={config.PWD}&action=1&format=1"
+
+s = connexion_api(urlId)
+
+r = appel_patstat("https://publication.epo.org/raw-data/products/86/editions/6891/files")
+
+listUrl = extraction_url(r)
+
+listNoms = liste_noms(listUrl)
+
+telechargement_ecriture(listUrl, listNoms)
