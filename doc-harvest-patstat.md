84de91cf75df488c6403a82b7dfa25729c108ed2
juliaGrandhay <julia.grandhay@gmail.com>
2021-10-07 17:50:36 +0200

    fin p03 et description p03 doc

--- doc-harvest-patstat.md
+++ doc-harvest-patstat.md
@@ -1,34 +1,59 @@
 # Documentation des traitements de la base de données PATSTAT
 
 ## Organisation des programmes
+
 <ol>
 <li>collectePatstatComplete : requête la base de données PATSTAT et télécharge tous les fichiers zippés de la base</li>
 <li>dezippage : dézippe tous les fichiers zippés de Patstat et range les fichiers CSV issus du dézippage dans le dossier correspondant à leur table PATSTAT</li>
 <li>csv_files_querying : permet de charger les CSV issus de PATSTAT dans Python par morceau, tout en appliquant des filtres - permet de charger les fichiers et de s'en servir en tant que dataframe</li>
 <li>dtypes_patstat_declaration : type des données des fichiers CSV issus de PATSTAT - facilite la création des dataframes</li>
-<li>p01_family_scope : limite la sélection aux demandes françaises. Inputs = tls 201, 206 et 207. Outputs = family_scope et patent_scope</li>
+<li>p01_family_scope : limite la sélection aux demandes françaises. Inputs = tls 201, 206 et 207. Outputs = docdb_family_scope et patent_scope</li>
 <li>p02_titles_abstracts : extrait les titres et les résumés des demandes. Inputs = patent_scope, tls 202 et 203. Outputs = titles et abstracts</li>
-<li>p03_patents : </li>
+<li>p03_patents : extrait l'information sur les publications et consolide le jeu de données = aux demandes françaises sont ajoutés :
+<ul>
+<li>phase européeen en cours ou qui a eu lieu</li>
+<li>phase internationale en cours ou qui a eu lieu</li>
+<li>informations sur la 1ère publication et le 1er octroi du brevet</li>
+<li>l'existence d'une priorité</li>
+<li>les informations concernant le titre et le résumé.</li>
+</ul>
+Inputs = patent_scope, abstracts, titles, tls 204 et tls 211. Outputs = publications et patent.
+</li>
+<li>p04_families</li>
 </ol>
-
 
 ## Notes
 
 ### Enregistrements fichiers
+
 <ul>
+<li>217 760 enregistrements dans docdb_family_scope</li>
 <li>581 733 enregistrements pour patent_scope</li>
-<li>579 217 enregistrements pour titles</li>
-<li>542 372 enregistrements pour abstracts</li>
+<li>579 217 enregistrements pour titles (titres des brevets)</li>
+<li>542 372 enregistrements pour abstracts (résumés des brevets)</li>
 <li>Manque des titres et des résumés pour les demandes. Ce ne sont pas les demandes artificielles car il n'y en a que 2 (appln_kind "D")</li>
 </ul>
 
-
 ### Dans patent_scope :
+
 <ul>
+<li>table qui donne les informations sur les demandes de brevet (application - appln)
+<ul>
+<li>appln_id : ID de la demande (valeur unique, normalement stable dans le temps depuis 2011 mais il peut arriver que cet ID soit changé)</li>
+<li>appln_auth : autorité nationale, régionale ou internationale en charge du traitement de la demande</li>
+<li>appln_nr : numéro de demande pour les brevets européens le cas échéan</li>
+<li>appln_kind : type de demande (brevet, modèle et certificat d'utilité)</li>
+<li>appln_filing_date et appln_filing_year : date de la demande</li>
+<li>appln_nr_epodoc : numéro de demande EPODOC de l'Office européen des brevets - &laquo; deprecated &raquo; - sera supprimé dans une des prochaines éditions de PATSTAT</li>
+<li>appln_nr_original : numéro de demande originel</li>
+<li>ipr_type : domaine de propriété intellectuelle couvert par la demande (brevet, modèle </li>
+<li>receiving_office : organisation internationale où la demande a été enregistrée le cas échéant</li>
+<li></li>
+</ul></li>
 <li>valeurs de appln_kind = "A", "C", "D", "F", "K", "L", "T", "U", "W".</li>
 <li>"D" est pour les demandes &laquo; artifielles &raquo;, càd dont PATSTAT n'a pas la trace en tant que telles mais pour lesqeulles il existe une priorité</li>
 <li>Application kind-codes D and Q : Application kind-codes 'D' and 'Q' identify "dummy" applications. Distinction between 'D' and 'Q' is made to help identify the corrective action required : Issues involving application kind-code 'D' can be resolved by an automated back-file correction exercise. Issues involving application kind-code 'Q' need intellectual effort and are being tackled manually, one by one.</li>
-<li>Application kind-codes K, L, M, N, OA limited number of countries, e.g. MC PH RU SU, supply identical application-identifications for separate publications. In order to resolve that issue, kind-code 'K' is allocated to the first duplicate encountered for a given application-identification, 'L' to the second etc.</li>
+<li>Application kind-codes K, L, M, N, O. A limited number of countries, e.g. MC PH RU SU, supply identical application-identifications for separate publications. In order to resolve that issue, kind-code 'K' is allocated to the first duplicate encountered for a given application-identification, 'L' to the second etc.</li>
 <li>French applications with kind-codes E, F, M : The kind-code of these French applications should be 'A'.Applications with kind-code E, F or M have been loaded with an incorrect kind-code at the time.</li>
 <li>appln_nr_original vide si demande artificielle ou si pas trouvée dans DOCDB</li>
 <li>receiving_office : bureau où la demande internationale a été effectuée - vide si demande nationale ou régionale</li>
@@ -36,17 +61,20 @@
 Routes possibles d'une demande : <br>
 
 ------------------------------------------------------
+
 |International phase | Regional phase| National phase|
 |--------------------|---------------|---------------|
 | Y/N/N              | Y/Y/N         | Y/Y/Y         |
 |                    |               | Y/N/Y         |
 |                    | N/Y/N         | N/Y/Y         |
 |                    |               | N/N/Y         |
+
 ------------------------------------------------------
 <br>
 Les combinaisons disponibles dans patent_scope sont : <br>
 
 -------------------------------------------------------------
+
 |International phase | Regional phase| National phase|Nombre |
 |--------------------|---------------|---------------|-------|
 | N                  |               | Y             | 129   |
@@ -58,47 +86,17 @@ Les combinaisons disponibles dans patent_scope sont : <br>
 | Y                  | N             | Y             | 173951|
 | Y                  | Y             | N             | 54650 |
 | Y                  | Y             | Y             | 14534 |
+
 --------------------------------------------------------------
 </li>
 </ul>
 
-### Dans abstracts :
-Il y a des valeurs manquantes dans appln_abstract - 3 dans le cas du jeu de données PATSTAT Spring 2021 (appln_id : 446022198, 503487228 et 503602912).
-Comme pour les titres, l'anglais est la langue par défaut : les autres langues n'apparaissent que si un résumé en anglais n'est pas disponible.
-
---------------------------------
-|langues|occurences|pourcentage|
-|-------|----------|-----------|
-|en|344927|63,60|
-|fr|133354|24,59|
-|ja|15936|2,94|
-|es|15634|2,88|
-|ko|12264|2,26|
-|pt|11413|2,10|
-|de|4074|0,75|
-|ru|1713|0,32|
-|zh|1206|0,22|
-|tr|673|0,12|
-|uk|321|0,06|
-|el|299|0,06|
-|no|241|0,04|
-|sr|109|0,02|
-|ar|77|0,01|
-|pl|49|0,01|
-|nl|39|0,01|
-|sv|13|0,00|
-|da|11|0,00|
-|ro|9|0,00|
-|hr|3|0,00|
-|cs|2|0,00|
-|me|2|0,00|
-|fi|1|0,00|
-|it|1|0,00|
-|lv|1|0,00|
+###  
 
 ### Dans titles :
-Aucune valeur manquante. 69,24 % des titres en anglais et 19,19 % en français. Anglais comme langue par défaut : les titres dans les autres langues ne sont considérés que s'il n'existe pas de titre en anglais.
-Langues des titres :
+
+Aucune valeur manquante. 69,24 % des titres en anglais et 19,19 % en français. Anglais comme langue par défaut : les
+titres dans les autres langues ne sont considérés que s'il n'existe pas de titre en anglais. Langues des titres :
 <ul>
 <li>tr = turc</li>
 <li>el = grec</li>
@@ -128,31 +126,76 @@ Langues des titres :
 </ul>
 <br>
 
--------------------
-|langues|occurences|
-|-------|----------|
-|en     |401076    |
-|fr     |111173    |
-|ja     |19493     |
-|es     |16865     |
-|pt     |13258     |
-|ko     |4556      |
-|de     |3641      |
-|da     |3194      |
-|ru     |2851      |
-|zh     |1205      |
-|tr     |673       |
-|it     |321       |
-|el     |300       |
-|no     |226       |
-|uk     |199       |
-|nl     |59        |
-|sv     |57        |
-|ar     |56        |
-|fi     |6         |
-|pl     |3         |
-|lt     |2         |
-|et     |1         |
-|hr     |1         |
-|he     |1         |
---------------------
+--------------------------------
+
+|langues|occurences|pourcentage|
+|-------|----------|-----------|
+|en     |401076    |69,24      |
+|fr     |111173    |19,19      |
+|ja     |19493     |3,37       |
+|es     |16865     |2,91       |
+|pt     |13258     |2,29       |
+|ko     |4556      |0,79       |
+|de     |3641      |0,63       |
+|da     |3194      |0,55       |
+|ru     |2851      |0,49       |
+|zh     |1205      |0,21       |
+|tr     |673       |0,12       |
+|it     |321       |0,06       |
+|el     |300       |0,05       |
+|no     |226       |0,04       |
+|uk     |199       |0,03       |
+|nl     |59        |0,01       |
+|sv     |57        |0,01       |
+|ar     |56        |0,01       |
+|fi     |6         |0,00       |
+|pl     |3         |0,00       |
+|lt     |2         |0,00       |
+|et     |1         |0,00       |
+|hr     |1         |0,00       |
+|he     |1         |0,00       |
+
+-------------------------------
+
+### Dans abstracts :
+
+Il y a des valeurs manquantes dans appln_abstract - 3 dans le cas du jeu de données PATSTAT Spring 2021 (appln_id :
+446022198, 503487228 et 503602912). Comme pour les titres, l'anglais est la langue par défaut : les autres langues
+n'apparaissent que si un résumé en anglais n'est pas disponible. langues des titres :
+<ul>
+<li>sr : serbe</li>
+<li>ro : roumain</li>
+<li>cs : tchèque</li>
+<li>me : monténégrin</li>
+</ul>
+
+--------------------------------
+
+|langues|occurences|pourcentage|
+|-------|----------|-----------|
+|en|344927|63,60|
+|fr|133354|24,59|
+|ja|15936|2,94|
+|es|15634|2,88|
+|ko|12264|2,26|
+|pt|11413|2,10|
+|de|4074|0,75|
+|ru|1713|0,32|
+|zh|1206|0,22|
+|tr|673|0,12|
+|uk|321|0,06|
+|el|299|0,06|
+|no|241|0,04|
+|sr|109|0,02|
+|ar|77|0,01|
+|pl|49|0,01|
+|nl|39|0,01|
+|sv|13|0,00|
+|da|11|0,00|
+|ro|9|0,00|
+|hr|3|0,00|
+|cs|2|0,00|
+|me|2|0,00|
+|fi|1|0,00|
+|it|1|0,00|
+|lv|1|0,00|
--- p03_patents.py
+++ p03_patents.py
@@ -18,9 +18,16 @@ DICT = {"tls204": {"sep": ",", "chunksize": 5000000, "dtype": types.tls204_types
 os.chdir(DATA_PATH)
 
 
-# On crée les variables des phases de dossiers et de délivrance
-
 def add_application_phases(patent_table: pd.DataFrame) -> pd.DataFrame:
+    """
+    np.where : if x==y: z else: a
+    np.where...N, 0, 1 -> missing values are considered as Y and get 1
+    np.where...Y, 1, 0 -> missing values are considered as N and get 0
+    copy to avoid side effect
+    :param patent_table: patent_scope
+    :return: dataframe where reg_phase, nat_phase are turned into dummy variables & where 2 other dummy variables
+    are created : oeb (in or have been in EP phase) international (in or have been in international phase)
+    """
     pat_phase = patent_table.copy()
     pat_phase["reg_phase"] = np.where(pat_phase["reg_phase"] == "N", 0, 1)
     pat_phase["nat_phase"] = np.where(pat_phase["nat_phase"] == "N", 0, 1)
@@ -30,40 +37,32 @@ def add_application_phases(patent_table: pd.DataFrame) -> pd.DataFrame:
     return pat_phase
 
 
-# On filtre sur la table T211 des publications toutes les publis liées à nos brevets
-
 def get_publications_from_appln_publn(t211directory: str, patent_table: pd.DataFrame) -> pd.DataFrame:
-    set_applnid = patent_table["appln_id"]
-    set_publnid = patent_table["earliest_pat_publn_id"]
+    """
+    :param t211directory: directory where tls 211 files are
+    :param patent_table: output from add_application_phases
+    :return: dataframe with publication info for French application ID
+    """
+    print("Start get publication from application ID")
 
     def query_publications_from_appln_publn(chunk: pd.DataFrame) -> pd.DataFrame:
-        publn_chunk = chunk[(chunk["appln_id"].isin(set_applnid)) |
-                            (chunk["pat_publn_id"].isin(set_publnid))]
+        publn_chunk = chunk[(chunk["appln_id"].isin(patent_table["appln_id"])) |
+                            (chunk["pat_publn_id"].isin(patent_table["earliest_pat_publn_id"]))]
         return publn_chunk
 
     _publications = cfq.multi_csv_files_querying(t211directory, query_publications_from_appln_publn,
                                                  DICT.get(t211directory))
 
+    print("End get publication from application ID")
+
     return _publications
 
 
-# Dans cette table "publications", publications filtrées sur notre périmètre, on récupère les informations
-# liées au dépôt : première publication, identifiée dans la table T201 par la varialbe "earliest_pat_publn_id":
-# on récupère l"identifiant "publn_id" lié à cette première publication ainsi que l"identifiant international "publn_nr"
-# et également la date de prmière publication.
-
-def get_first_publication(patent_table: pd.DataFrame, publn_table: pd.DataFrame) -> pd.DataFrame:
-    """ This function gets from an application table and a publication table the information on the earliest publication
-
-    param patent_table:  a table with application identifier and related first publication identifier
-    type patent_table: dataframe
-    param publn_table:  Table with publication informations - T211 or part of
-    type publn_table: dataframe
-
-    :return: a dataframe with added publication informations : the patstat publication identifier,
-    the international publication identifier and the publication date,
-    all related to the earliest publication of the application
-
+def get_first_application(patent_table: pd.DataFrame, publn_table: pd.DataFrame) -> pd.DataFrame:
+    """
+    param patent_table:  output from add_application_phases
+    param publn_table:  output from get_publications_from_appln_publn
+    :return: a dataframe with publication info for the first application registered
     """
 
     depot = pd.merge(patent_table[["appln_id", "earliest_pat_publn_id"]],
@@ -78,15 +77,10 @@ def get_first_publication(patent_table: pd.DataFrame, publn_table: pd.DataFrame)
 
 
 def get_first_grant(patent_table: pd.DataFrame, publn_table: pd.DataFrame) -> pd.DataFrame:
-    """  This function gets from an application table and a publication table the first grant"s information
-
-    param patent_table:  a table with application identifier
-    type patent_table: dataframe
-    param publn_table:  Table with publication informations - T211 or part of
-    type publn_table: dataframe
-
-    :return: a dataframe with grant informations related to application identifiers "appln_id"
-
+    """
+    param patent_table:  output from add_application_phases
+    param publn_table:  output from get_publications_from_appln_publn
+    :return: a dataframe with grant info for the first application registered
     """
 
     grant = pd.merge(patent_table[["appln_id"]],
@@ -103,87 +97,90 @@ def get_first_grant(patent_table: pd.DataFrame, publn_table: pd.DataFrame) -> pd
 
 
 def get_priorities(t204directory: str, patent_table: pd.DataFrame) -> pd.DataFrame:
-    """ This function gets from an application table and a repertory with priority tables the informations on priorities
-
-    param t204directory:   path of directory containing one or a few patstat"s priority tables (csv)
-    type t204directory: string
-    param patent_table:  Table with application informations - T211 or part of - must have "appln_id"
-    type patent_table: dataframe
-    param nrows: allows to specify a number of lines only to extract for tests
-    type nrows: integer
-
-    :return: a dataframe, an extract of T204 table for the application identifiers present in patent_table loaded
-
+    """
+    param t204directory:   directory where tls 204 files are
+    param patent_table:  output from add_application_phases
+    :return: dataframe with the French applications which have a priority claim
     """
 
-    set_applnid = patent_table["appln_id"]
+    print("Start get priorities from application ID")
 
     def lect_patstat_table_from_appln_id(chunk: pd.DataFrame) -> pd.DataFrame:
-        """   This query extracts from any patstat table the informations related to the patent applications in
-        "set_patent"
-
-        param chunk:   any dataframe containing the application identifier "appln_id"
-        type chunk: dataframe
-        param set_patent:  set containing the applications identifiers "appln_id" to keep
-        type set_patent: set
-
-        :return: a dataframe, an extract of T204 table for the application identifiers present in patent_table loaded
+        """
+        param chunk: a chunk from CSV
+        :return: rows with the corresponding application ID
 
         """
 
-        query = chunk[chunk["appln_id"].isin(set_applnid)]
+        query = chunk[chunk["appln_id"].isin(patent_table["appln_id"])]
 
         return query
 
     t204 = cfq.multi_csv_files_querying(t204directory, lect_patstat_table_from_appln_id, DICT.get(t204directory))
 
+    print("Start get priorities from application ID")
+
     return t204
 
 
-def get_ispriority_byfamily(patent_table: pd.DataFrame) -> pd.DataFrame:
-    """   This function creates from an application table the indicator on the priority in the family
-
-    param patent_table:  Table with application informations - T211 or part of - must have "appln_id" identifier and
-    family identifier "docdb_family_id"
-    type patent_table: dataframe
-
-    :return: a dataframe with a boolean variable added indicating if the application is a priority in his family or not.
-
+def get_ispriority_byfamily(_priority_table: pd.DataFrame, patent_table: pd.DataFrame) -> pd.DataFrame:
     """
-    priority_table = get_priorities("tls204", patent_table)
-    # cette étape récupère les priorités d"une application
+    :param _priority_table: output from get_priority
+    :param patent_table: output from add_application_phases
+    :return: dataframe
+    """
+
+    # we keep only applications and their DOCDB family with priorities
+    # prior_appln_id refers to the application of which the priority is claimed
     prior = pd.merge(patent_table[["appln_id", "docdb_family_id"]],
-                     priority_table[["appln_id", "prior_appln_id"]],
+                     _priority_table[["appln_id", "prior_appln_id"]],
                      how="inner", on="appln_id") \
         .rename(columns={"docdb_family_id": "fam", "appln_id": "appln_id_orig"})
 
-    # cette étape récupère la famille de la priorité
+    # by merging on the tls 204 prior_appln_id and tls 201 appln_id, we get the DOCDB family of the original application
     prior_fam = pd.merge(prior, patent_table[["appln_id", "docdb_family_id"]],
                          how="left", left_on="prior_appln_id", right_on="appln_id") \
         .rename(columns={"docdb_family_id": "fam_prior"}) \
         .drop(columns=["appln_id"])
 
-    # cette étape filtre les priorités seulement sur les priorités de la même famille
-    set_priority = set(prior_fam[prior_fam["fam"] == prior_fam["fam_prior"]]["prior_appln_id"])
+    # get prior_appln_id for applications which have the same DOCDB family
+    # both in the original and subsequent application
+    set_priority = prior_fam[prior_fam["fam"] == prior_fam["fam_prior"]]["prior_appln_id"]
 
+    # create dataframe with appln_id and a dummy variable "ispriority" with 1 if both the original
+    # and subsequent application have the same DOCDB family, else 0
     _ispriority = patent_table.copy()[["appln_id"]]
     _ispriority["ispriority"] = np.where(_ispriority["appln_id"].isin(set_priority), 1, 0)
 
     return _ispriority
 
 
-patent_scope = pd.read_csv("patent_scope.csv", sep="|", dtype=types.tls201_types)
-titles = pd.read_csv("titles.csv", sep="|", dtype={"appln_id": np.int64})
-abstracts = pd.read_csv("abstracts.csv", sep="|", dtype={"appln_id": np.int64})
-pat = add_application_phases(patent_scope)
-publications = get_publications_from_appln_publn("tls211", pat)
-# publications.to_csv("publications.csv", sep="|", index=False)
-first_application = get_first_publication(pat, publications)
-first_grant = get_first_grant(pat, publications)
-ispriority = get_ispriority_byfamily(pat)
-patent = pd.merge(pat, first_application, how="left", on="appln_id").merge(first_grant, how="left",
-                                                                           on="appln_id").merge(ispriority, how="left",
-                                                                                                on="appln_id").merge(
-    titles, how="left", on="appln_id").merge(abstracts, how="left", on="appln_id")
+def main():
+    patent_scope = pd.read_csv("patent_scope.csv", sep="|", dtype=types.tls201_types)
+    titles = pd.read_csv("titles.csv", sep="|", dtype={"appln_id": np.int64})
+    abstracts = pd.read_csv("abstracts.csv", sep="|", dtype={"appln_id": np.int64})
+    pat = add_application_phases(patent_scope)
+    publications = get_publications_from_appln_publn("tls211", pat)
+    publications.to_csv("publications.csv", sep="|", index=False)
+    first_application = get_first_application(pat, publications)
+    first_grant = get_first_grant(pat, publications)
+    priority_table = get_priorities("tls204", pat)
+    ispriority = get_ispriority_byfamily(priority_table, pat)
+    patent = pd.merge(pat,
+                      first_application,
+                      how="left",
+                      on="appln_id").merge(first_grant,
+                                           how="left",
+                                           on="appln_id").merge(ispriority,
+                                                                how="left",
+                                                                on="appln_id").merge(titles,
+                                                                                     how="left",
+                                                                                     on="appln_id").merge(abstracts,
+                                                                                                          how="left",
+                                                                                                          on="appln_id")
 
-# patent.to_csv("patent.csv", sep="|", index=False)
+    patent.to_csv("patent.csv", sep="|", index=False)
+
+
+if __name__ == "__main__":
+    main()
