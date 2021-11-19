be1023f1aa49301c6b69d45764684cbf0b5d69d2
juliaGrandhay <julia.grandhay@gmail.com>
2021-11-19 18:48:10 +0000

    Choix model fasttext 1 avec oversampling + 1 type par doc_std_name et name_source

---
+++ classification_nom.py
@@ -0,0 +1,178 @@
+import string, re
+import pickle
+from project.server.main.FoR import set_FoR
+from project.server.main.utils import download_file, get_aggregate
+from project.server.main.utils_swift import conn, upload_object, download_object
+import pandas as pd
+import fasttext
+from sklearn.model_selection import train_test_split
+from project.server.main.utils_str import normalize
+import os
+from project.server.main.logger import get_logger
+
+logger = get_logger(__name__)
+
+PV_MOUNT = "/src/local_data/"
+os.system(f"mkdir -p {PV_MOUNT}")
+
+def calibrate_pubmed(is_stratified, sample_data_file):
+    try:
+        issn_dict_health = pickle.load(open(f"{PV_MOUNT}issn_dict_health.pkl", "rb"))
+        assert(len(issn_dict_health)>1000)
+    except:
+        logger.debug("getting FoR")
+        set_FoR()
+        #download_object("models", "issn_dict_health.pkl", f"{PV_MOUNT}issn_dict_health.pkl")
+    issn_dict_health = pickle.load(open(f"{PV_MOUNT}issn_dict_health.pkl", "rb"))
+
+    calibrate("pubmed", issn_dict_health, is_stratified, sample_data_file = sample_data_file)
+
+def dualize_dict(input_dict, is_global):
+    new_dict = {}
+    for k in input_dict:
+        for e in input_dict[k]:
+            current_key = e
+            if is_global:
+                current_key = 'global'
+            if current_key not in new_dict:
+                new_dict[current_key] = []
+            if k not in new_dict[current_key]:
+                new_dict[current_key].append(k)
+    return new_dict
+
+def sample(collection, issn_map, is_stratified):
+    if is_stratified:
+        sampling_size = 50000 # per category
+        field_map = dualize_dict(issn_map, is_global = False)
+    else:
+        sampling_size = 1000000
+        field_map = dualize_dict(issn_map, is_global = True)
+    data_field = []
+    tmp_file_to_del = []
+    sample_data = f"{PV_MOUNT}sample_data_{collection}_strat{is_stratified}_{sampling_size}.json"
+    for field in field_map:
+        issns = field_map[field]
+        pipeline = [
+            {
+            "$match": {
+            "$or": [
+                { "issn_electronic": { "$in": issns}},
+                { "issn_print": { "$in": issns}} ]
+            }
+        },
+        { "$sample": { "size": sampling_size } },
+        {
+            "$project": {
+                "_id": 0, "title": 1, "abstract": 1, "keywords": 1, "issn_electronic": 1, "issn_print": 1, "mesh_headings": 1, "journal_title": 1
+            }
+        }
+        ]
+        url_data = get_aggregate(collection, pipeline, field.replace(' ',''))
+        tmp_file_to_del.append(url_data.split('/')[-1])
+        layer_data = f"{PV_MOUNT}layer_data.json"
+        download_object("tmp", url_data.split('/')[-1], layer_data)
+        os.system(f"cat {layer_data} >> {sample_data}")
+        os.system(f"rm -rf {layer_data}")
+        #data_field.append(pd.read_json(sample_data, orient="records", lines=True).to_dict(orient='records'))
+    #data = pd.concat(data_field)
+    upload_object("sampling", sample_data)
+
+
+    for f in tmp_file_to_del:
+        conn.delete_object("tmp", f)
+
+    return sample_data
+
+
+def calibrate(collection, issn_map, is_stratified, sample_data_file = None):
+    if sample_data_file is None:
+        sample_data = sample(collection, issn_map, is_stratified)
+    else:
+        sample_data = f"{PV_MOUNT}{sample_data_file}"
+        download_object("sampling", sample_data_file, sample_data)
+    data = pd.read_json(sample_data, orient="records", lines=True).to_dict(orient='records')
+    #download_object("tmp", sample_data.split('/')[-1], sample_data)
+    logger.debug("len data = "+str(len(data)))
+    logger.debug("len issn_map = "+str(len(issn_map)))
+    for elt in data:
+        if '_id' in elt:
+            del elt['_id']
+        current_label_text = []
+        #current_label_text_global = []
+        for issn_type in ['issn_electronic', 'issn_print']:
+            issn = elt[issn_type]
+            if issn in issn_map:
+                current_label_text += issn_map[issn]
+
+        current_label_text = list(set(current_label_text))
+
+        elt["labels_text"] = current_label_text
+
+    data_with_label = [ e for e in data if len(e['labels_text'])]
+
+    data_train, data_test = train_test_split(data_with_label, test_size = 85000, random_state = 0)
+
+    for data_type in ["train", "test"]:
+        logger.debug(data_type)
+        outfile = {}
+        for f in ['title', 'abstract', 'keywords', 'mesh_headings', 'journal_title']:
+            outfile[f] = open(f"{PV_MOUNT}{collection}_{data_type}_{f}.txt", "w")
+            outfile[f].close()
+
+        for f in ['title', 'abstract', 'keywords', 'mesh_headings', 'journal_title']:
+            outfile[f] = open(f"{PV_MOUNT}{collection}_{data_type}_{f}.txt", "a+")
+            logger.debug(f)
+
+            if data_type == "train":
+                current_data = data_train
+            else:
+                current_data = data_test
+
+            for ix, elt in enumerate(current_data):
+                if ix % 100000 == 0:
+                    logger.debug(ix)
+
+                current_words = elt.get(f)
+                if current_words is None:
+                    continue
+
+                if isinstance(current_words, list):
+                    current_words = " ".join(current_words)
+
+                if f == "abstract" and len(current_words.split(" ")) < 20:
+                    continue
+                elif f == "title" and len(current_words.split(" ")) < 10:
+                    continue
+                elif len(current_words.split(" ")) < 2:
+                    continue
+                elif len(current_words) < 5:
+                    continue
+
+                current_words = normalize(current_words)
+
+                labels = ["__label__" + label.replace(' ','_') for label in elt.get('labels_text', [])]
+
+                tags = " ".join(labels)
+
+                newline = current_words + " " + tags + "\n"
+
+                outfile[f].write(newline)
+            outfile[f].close()
+
+    for f in ['journal_title', 'title', 'abstract', 'keywords', 'mesh_headings']:
+        logger.debug("training "+f)
+
+        model = fasttext.train_supervised(f'{PV_MOUNT}{collection}_train_{f}.txt',
+                                       wordNgrams = 2,
+                                       minCount = 20,
+                                       loss='ova',
+                                       epoch = 50)
+        model_filename = f"{PV_MOUNT}{collection}_model_{f}_strat{is_stratified}.model"
+        model.save_model(model_filename)
+        upload_object("models", model_filename)
+
+        test = model.test(f'{PV_MOUNT}{collection}_test_{f}.txt', k=-1, threshold=0.5)
+        precision = test[1]
+        recall = test[2]
+        f1 = 2*(recall * precision) / (recall + precision)
+        logger.debug(f"precision: {precision}, recall: {recall}, f1: {f1}")
---
+++ comparaison.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import csv_files_querying as cfq
+import dtypes_patstat_declaration as types
+import fasttext
+from imblearn.over_sampling import RandomOverSampler
+import numpy as np
+import os
+import pandas as pd
+import re
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+
+# directory where the files are
+DATA_PATH = "/run/media/julia/DATA/test/"
+DICT = {"tls207": {'sep': ',', 'chunksize': 10000000, 'dtype': types.tls207_types},
+        "tls206": {'sep': ',', 'chunksize': 3000000, 'dtype': types.tls206_types}
+        }
+
+PM = ["COMPANY",
+      "GOV NON-PROFIT",
+      "UNIVERSITY",
+      "HOSPITAL",
+      "GOV NON-PROFIT UNIVERSITY",
+      "COMPANY GOV NON-PROFIT",
+      "COMPANY UNIVERSITY",
+      "COMPANY HOSPITAL"]
+
+# set working directory
+os.chdir(DATA_PATH)
+
+patents = pd.read_csv("patent.csv", sep="|", dtype=types.patent_types)
+old_part = pd.read_csv("part_init.csv",
+                       sep='|',
+                       dtype=types.part_init_types,
+                       encoding="utf-8")
+
+appln_id = pd.DataFrame(patents["appln_id"].append(old_part["appln_id"])).drop_duplicates()
+
+tls207 = cfq.filtering("tls207", appln_id, "appln_id", DICT["tls207"])
+tls206 = cfq.filtering("tls206", tls207, "person_id", DICT["tls206"])
+new_participants = pd.merge(tls207, tls206, how="inner", on="person_id")
+
+tls206_c = new_participants.dropna(subset=["person_name"])
+
+tls206_c = tls206_c.copy()
+
+tls206_c["label"] = tls206_c["psn_sector"].apply(
+    lambda a: 'pm' if a in set(PM) else 'pp' if a == "INDIVIDUAL" else np.nan)
+
+lrn = tls206_c[tls206_c["label"].notna()].drop(
+    columns=["applt_seq_nr", "person_id", "appln_id", "doc_std_name_id", "nuts", "nuts_level", "doc_std_name_id",
+             "psn_id", "psn_level", "psn_sector", "han_id",
+             "han_harmonized"]).drop_duplicates()
+
+train, test = train_test_split(lrn, random_state=123, shuffle=True)
+ros = RandomOverSampler(random_state=42)
+train_sample = train.copy()
+train_ros_X, train_ros_Y = ros.fit_resample(train_sample.iloc[:, :-1], train_sample["label"])
+train_ros = train_ros_X.join(train_ros_Y)
+
+# FASTEXT
+# sans invt
+train_file = open('train.txt', 'w')
+for i, row in train_ros.iterrows():
+    my_line = f"__label__{row.label} {row.doc_std_name} {row.person_name} \n"
+    train_file.write(my_line)
+train_file.close()
+
+test_file = open('test.txt', 'w')
+for i, row in test.iterrows():
+    my_line = f"__label__{row.label} {row.doc_std_name} {row.person_name} \n"
+    test_file.write(my_line)
+test_file.close()
+
+model = fasttext.train_supervised('train.txt',
+                                  wordNgrams=2,
+                                  minCount=20,
+                                  loss='ova',
+                                  epoch=50)
+
+# avec invt
+train_file2 = open('train2.txt', 'w')
+for i, row in train_ros.iterrows():
+    my_line = f"__label__{row.label} {row.doc_std_name} {row.person_name} str({row.invt_seq_nr}) \n"
+    train_file2.write(my_line)
+train_file2.close()
+
+test_file2 = open('test2.txt', 'w')
+for i, row in test.iterrows():
+    my_line = f"__label__{row.label} {row.doc_std_name} {row.person_name} str({row.invt_seq_nr}) \n"
+    test_file2.write(my_line)
+test_file2.close()
+
+model2 = fasttext.train_supervised('train2.txt',
+                                   wordNgrams=2,
+                                   minCount=20,
+                                   loss='ova',
+                                   epoch=50)
+
+model.save_model('model_test')
+model2.save_model("model_test2")
+test_pred = model.test('test.txt', k=-1, threshold=0.5)
+test_pred2 = model2.test('test2.txt', k=-1, threshold=0.5)
+test_fsttxt = test.copy()
+test_fsttxt["txt"] = test_fsttxt["doc_std_name"] + " " + test_fsttxt["person_name"]
+test_fsttxt["prediction"] = test_fsttxt["txt"].apply(lambda a: model.predict(a))
+test_fsttxt["type"] = test_fsttxt["prediction"].apply(lambda a: re.search("pp|pm", str(a)).group(0))
+test_fsttxt = test_fsttxt.copy()
+test_fsttxt["txt_invt"] = test_fsttxt["doc_std_name"] + " " + test_fsttxt["person_name"] + " " + test_fsttxt[
+    "invt_seq_nr"].astype(str)
+test_fsttxt["pred_invt"] = test_fsttxt["txt_invt"].apply(lambda a: model2.predict(a))
+test_fsttxt["type_int"] = test_fsttxt["pred_invt"].apply(lambda a: re.search("pp|pm", str(a)).group(0))
+test_fsttxt["invt"] = test_fsttxt["invt_seq_nr"].apply(lambda a: "pm" if a == 0 else "pp")
+test_fsttxt.to_csv("predictions_fasttext.csv", sep=";", encoding="utf-8", index=False)
+
+# XGB
+test_xgb = test.copy()
+train_xgb = train_ros.copy()
+
+test_xgb["label2"] = test_xgb["label"].apply(
+    lambda a: 0 if a == "pm" else 1)
+
+test_xgb = test_xgb.drop(columns="label").rename(columns={"label2": "label"}).copy()
+
+train_xgb["label2"] = train_xgb["label"].apply(
+    lambda a: 0 if a == "pm" else 1)
+
+train_xgb = train_xgb.drop(columns="label").rename(columns={"label2": "label"}).copy()
+
+colonnes = ['person_name', 'person_name_orig_lg', 'person_address',
+            'person_ctry_code', 'doc_std_name', 'psn_name',
+            'han_name']
+
+for col in colonnes:
+    train_xgb[col] = train_xgb[col].astype("category")
+    test_xgb[col] = test_xgb[col].astype("category")
+
+dtrain = xgb.DMatrix(train_xgb[:, :-1], train_xgb["label"], enable_categorical=True)
+dtest = xgb.DMatrix(test_xgb.iloc[:, :-1], test_xgb["label"], enable_categorical=True)
+
+param = {'max_depth': 2, 'objective': 'binary:logistic', 'seed': 123, 'nthread': 2, 'eval_metric': 'auc'}
+evallist = [(dtest, 'eval'), (dtrain, 'train')]
+num_round = 10
+bst = xgb.train(param, dtrain, num_round, evallist)
+bst.save_model('0001.model')
+
+ypred = bst.predict(dtest)
+
+test_xgb = test_xgb.reset_index().copy()
+
+test_xgb.insert(9, "pred", pd.Series(ypred))
+
+test_xgb["type"] = test_xgb["pred"].apply(lambda a: "pp" if a >= 0.5 else "pm")
+test_xgb["perso"] = test_xgb["label"].apply(lambda a: "pp" if a == 1.0 else "pm")
+test_xgb["invt"] = test_xgb["invt_seq_nr"].apply(lambda a: "pm" if a == 0 else "pp")
+test_xgb = test_xgb.drop(columns="index")
+test_xgb.to_csv("prediction_xgb.csv", sep=";", encoding="utf-8", index=False)
+
+fsttxt = pd.read_csv("predictions_fasttext.csv", sep=";", encoding="utf-8")
+fsttxt = fsttxt.drop(columns=["txt", "prediction", "txt_invt", "pred_invt"]).rename(
+    columns={"type": "type_fsttxt1", "type_int": "type_fsttxt_invt"}).copy()
+gb = pd.read_csv("prediction_xgb.csv", sep=";", encoding="utf-8")
+gb = gb.drop(columns=["label", "pred"]).rename(columns={"perso": "label", "type": "type_xgb"}).copy()
+
+comp = pd.merge(fsttxt, gb, on=["invt_seq_nr", "person_name", "doc_std_name", "label", "invt"], how="inner")
+comp["test_invt"] = np.where(comp["label"] != comp["invt"], 1, 0)
+comp["test_fsttxt1"] = np.where(comp["label"] != comp["type_fsttxt1"], 1, 0)
+comp["test_fsttxt_invt"] = np.where(comp["label"] != comp["type_fsttxt_invt"], 1, 0)
+comp["test_xgb"] = np.where(comp["label"] != comp["type_xgb"], 1, 0)
+
+res = pd.DataFrame(data={"invt": [comp["test_invt"].sum()],
+                         "fsttxt1": [comp["test_fsttxt1"].sum()],
+                         "fsttxt_invt": [comp["test_fsttxt_invt"].sum()],
+                         "xgb": [comp["test_xgb"].sum()]})
