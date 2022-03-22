4a1754bebcbfea051d672ba8674c18831c2787fc
juliaGrandhay <julia.grandhay@gmail.com>
2022-03-22 14:10:33 +0100

    add print messages

--- application/server/main/tasks.py
+++ application/server/main/tasks.py
@@ -37,6 +37,7 @@ def create_task_all(args):
 def create_task_inpi():
     try:
         ad, nme = p00_outils_inpi_adress.unzip_inpi()
+        print("p00: success", flush=True)
     except:
         ad, nme = pd.DataFrame()
         error = sys.exc_info()[0]
@@ -48,7 +49,9 @@ def create_task_inpi():
 def create_task_harvest_patstat():
     try:
         ls_fl = collectePatstatComplete.main()
+        print("Collecte PATSTAT complete : success", flush=True)
         dezippage.main()
+        print("dezippage : success", flush=True)
     except:
         ls_fl = []
         error = sys.exc_info()[0]
@@ -60,13 +63,13 @@ def create_task_harvest_patstat():
 def create_task_p01_p04_patstat():
     try:
         p01_family_scope.get_patentscope()
-        print("p01: success")
+        print("p01: success", flush=True)
         p02_titles_abstracts.tit_abst()
-        print("p02: success")
+        print("p02: success", flush=True)
         p03_patents.get_pubpat()
-        print("p03: success")
+        print("p03: success", flush=True)
         p04_families.getfam()
-        print("p04: success")
+        print("p04: success", flush=True)
     except:
         error = sys.exc_info()[0]
         logger.error(f'Process PATSTAT caused an error : {error}')
@@ -75,6 +78,7 @@ def create_task_p01_p04_patstat():
 def create_task_p05_patstat():
     try:
         p05_creat_participants.start_part()
+        print("p05: success", flush=True)
     except:
         error = sys.exc_info()[0]
         logger.error(f'Process PATSTAT caused an error : {error}')
@@ -83,6 +87,7 @@ def create_task_p05_patstat():
 def create_task_p05b_patstat():
     try:
         p05b_clean_participants.get_clean_part()
+        print("p05b: success", flush=True)
     except:
         error = sys.exc_info()[0]
         logger.error(f'Process PATSTAT caused an error : {error}')
@@ -91,6 +96,7 @@ def create_task_p05b_patstat():
 def create_task_p06_indiv_patstat():
     try:
         p06_clean_participants_individuals.get_clean_ind()
+        print("p06: success", flush=True)
     except:
         error = sys.exc_info()[0]
         logger.error(f'Process PATSTAT caused an error : {error}')
@@ -110,6 +116,7 @@ def create_task_p07_entp_patstat():
 def create_task_p08_part_final_patstat():
     try:
         p08_participants_final.part_final()
+        print("p08: success", flush=True)
     except:
         error = sys.exc_info()[0]
         logger.error(f'Process PATSTAT caused an error : {error}')
@@ -126,6 +133,7 @@ def create_json_patent_scanr():
 def create_task_geo():
     try:
         p09_geoloc.geoloc()
+        print("p09: success", flush=True)
     except:
         error = sys.exc_info()[0]
         logger.error(f'Harvest PATSTAT caused an error : {error}')
