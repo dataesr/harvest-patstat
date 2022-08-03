fcec2b27750ab5f7be7d241921607b9919680c7e
juliaGrandhay <julia.grandhay@gmail.com>
2022-08-03 15:33:11 +0200

    script to create json file env techno

---
+++ patstat/comp_version_y02y04s.py
@@ -0,0 +1,269 @@
+# !/usr/bin/env python
+# coding: utf-8
+
+# Ce programme permet de créer le fichier sur "TECHNOLOGIES OR APPLICATIONS FOR MITIGATION OR ADAPTATION AGAINST CLIMATE
+# CHANGE" (classification Y02 CPC) et "SYSTEMS INTEGRATING TECHNOLOGIES RELATED TO POWER NETWORK OPERATION,
+# COMMUNICATION OR INFORMATION TECHNOLOGIES FOR IMPROVING THE ELECTRICAL POWER GENERATION, TRANSMISSION, DISTRIBUTION,
+# MANAGEMENT OR USAGE, i.e. SMART GRIDS" (classification Y04S CPC) pour au format JSON qui va alimenter ScanR
+
+
+import os
+import pandas as pd
+from utils import swift
+
+from patstat import dtypes_patstat_declaration as types
+
+DATA_PATH = os.getenv('MOUNTED_VOLUME_TEST')
+
+
+def jointure(fm, df):
+    fm2 = pd.merge(fm, df, left_on="docdb_family_id", right_on="family_id", how="left").drop(columns="family_id")
+    return fm2
+
+
+def to_date_str(x):
+    res = str(x)[0:10]
+    if len(res) == 10:
+        res = res + "T00:00:00"
+        return res
+    return None
+
+
+def get_year(x):
+    if isinstance(x, str):
+        return int(x[0:4])
+    return None
+
+
+def get_json_env():
+    # set working directory
+    os.chdir(DATA_PATH)
+    patent = pd.read_csv("patent.csv", sep="|", encoding="utf-8", dtype=types.patent_types,
+                         usecols=["appln_auth", "appln_publn_number", "internat_appln_id", "publn_kind", "key_appln_nr",
+                                  "docdb_family_id", "ispriority", "ipr_type", "appln_filing_date", "appln_publn_date",
+                                  "grant_publn_date", "appln_nr"])
+    patent["key_appln_nr"] = patent["key_appln_nr"].apply(lambda a: a.strip())
+    for f_date in ["appln_filing_date", "appln_publn_date", "grant_publn_date"]:
+        patent[f_date] = patent[f_date].apply(to_date_str)
+
+    # patent[["appln_filing_date", "appln_publn_date", "grant_publn_date"]] = patent[
+    #    ["appln_filing_date", "appln_publn_date", "grant_publn_date"]].apply(pd.to_datetime)
+
+    fam = pd.read_csv("families.csv", sep="|", encoding="utf-8", dtype=types.patent_types)
+    for f_date in ["earliest_publn_date", "earliest_application_date", "date_first_granted"]:
+        fam[f_date] = fam[f_date].apply(to_date_str)
+
+    # fam[["earliest_publn_date", "earliest_application_date", "date_first_granted"]] = fam[
+    #    ["earliest_publn_date", "earliest_application_date", "date_first_granted"]].apply(pd.to_datetime)
+    fam[["title_en", "title_fr", "title_default", "title_default_language", "abstract_en", "abstract_fr",
+         "abstract_default_language", "abstract_default"]] = fam[
+        ["title_en", "title_fr", "title_default", "title_default_language", "abstract_en", "abstract_fr",
+         "abstract_default_language", "abstract_default"]].fillna("")
+
+    for col in ["is_oeb", "is_international"]:
+        fam[col] = fam[col].apply(lambda a: True if a == 1 else False)
+
+    fam_techno = pd.read_csv("families_technologies.csv", sep="|", encoding="utf-8", dtype=types.patent_types)
+
+    part = pd.read_csv("part_p08.csv", sep="|", encoding="utf-8", dtype=types.partfin_types, engine="python")
+    part["key_appln_nr"] = part["key_appln_nr"].apply(lambda a: a.strip())
+    part["key_appln_nr_person"] = part["key_appln_nr_person"].replace(r"\s+", "", regex=True)
+    part["id_personne"] = part["id_personne"].replace(r"\s+", "", regex=True)
+    part["siren"] = part["siren"].fillna("")
+
+    role = pd.read_csv("role.csv", sep="|", encoding="utf-8")
+    role["key_appln_nr_person"] = role["key_appln_nr_person"].replace(r"\s+", "", regex=True)
+
+    # filtre sur les codes CPC portant sur les brevets consacrés aux technologies d'atténuation ou d'adaptation
+    # aux changements climatiques
+
+    fam_techno = fam_techno.loc[
+        fam_techno["code"].isin(["Y02A", "Y02B", "Y02C", "Y02D", "Y02E", "Y02P", "Y02T", "Y02W", "Y04S"])]
+    fam_techno = fam_techno.sort_values("code")
+
+    patent = patent.loc[patent["docdb_family_id"].isin(fam_techno["docdb_family_id"])]
+
+    fam = fam.loc[fam["docdb_family_id"].isin(fam_techno["docdb_family_id"])]
+
+    part = part.loc[part["docdb_family_id"].isin(fam_techno["docdb_family_id"])]
+
+    role = role.loc[role["key_appln_nr_person"].isin(part["key_appln_nr_person"])]
+
+    pat_dict = {}
+    for row in fam_techno.itertuples():
+        family_id = row.docdb_family_id
+        if family_id not in pat_dict:
+            pat_dict[family_id] = []
+        elt = {'level': row.level, "code": row.code, "type": "cpc", "label": {"default": row.libelle}}
+        if elt not in pat_dict[family_id]:
+            pat_dict[family_id].append(elt)
+
+    res = []
+    for family_id in pat_dict:
+        new_elt = {'family_id': family_id, 'domains': pat_dict[family_id]}
+        res.append(new_elt)
+
+    df_fam = pd.DataFrame(res)
+
+    patent["publicationNumber"] = patent["appln_publn_number"].astype(str) + patent["publn_kind"]
+    patent["links"] = patent["appln_auth"] + patent["publicationNumber"]
+    patent["internatApplicationNumber"] = patent["internat_appln_id"].apply(lambda a: "" if a == 0 else str(a))
+    patent["isPriority"] = patent["ispriority"].apply(lambda a: True if a == 1 else False)
+    pat_json = patent[
+        ["docdb_family_id", "key_appln_nr", "isPriority", "ipr_type", "appln_auth", "appln_filing_date", "appln_nr",
+         "internatApplicationNumber",
+         "appln_publn_date", "publicationNumber", "grant_publn_date", "links"]].rename(
+        columns={"key_appln_nr": "id", "ipr_type": "ipType", "appln_auth": "office",
+                 "appln_filing_date": "applicationDate",
+                 "appln_nr": "applicationNumber", "appln_publn_date": "publicationDate",
+                 "grant_publn_date": "grantedDate"})
+
+    links_dict = {}
+    for row in pat_json.itertuples():
+        family_id = row.links
+        if family_id not in links_dict:
+            links_dict[family_id] = []
+        elt = {"type": "", "url": f"https://worldwide.espacenet.com/patent/search/?q=pn%3D%22{row.links}%22",
+               "label": "espacenet"}
+        if elt not in links_dict[family_id]:
+            links_dict[family_id].append(elt)
+
+    res = []
+    for family_id in links_dict:
+        new_elt = {'family_id': family_id, 'links': links_dict[family_id]}
+        res.append(new_elt)
+
+    df_links = pd.DataFrame(res)
+
+    pat_json2 = pd.merge(pat_json, df_links, left_on="links", right_on="family_id", how="left").drop(
+        columns=["links_x", "family_id"]).rename(columns={"links_y": "links"})
+
+    pat_dict = {}
+    for row in pat_json2.itertuples():
+        family_id = row.docdb_family_id
+        if family_id not in pat_dict:
+            pat_dict[family_id] = []
+        elt = {'id': row.id, "isPriority": row.isPriority, "ipType": row.ipType, "office": row.office,
+               "applicationDate": row.applicationDate, "applicationNumber": row.applicationNumber,
+               "internatApplicationNumber": row.internatApplicationNumber, "pulicationDate": row.publicationDate,
+               "publicationNumber": row.publicationNumber, "grantedDate": row.grantedDate, "links": row.links}
+        if elt not in pat_dict[family_id]:
+            pat_dict[family_id].append(elt)
+
+    res = []
+    for family_id in pat_dict:
+        new_elt = {'family_id': family_id, 'patents': pat_dict[family_id]}
+        res.append(new_elt)
+
+    df_pat = pd.DataFrame(res)
+
+    title_dict = {}
+    for row in fam.itertuples():
+        family_id = row.docdb_family_id
+        if family_id not in title_dict:
+            title_dict[family_id] = []
+        elt = {"fr": row.title_fr, "en": row.title_en, "default": row.title_default}
+        if elt not in title_dict[family_id]:
+            title_dict[family_id].append(elt)
+
+    res = []
+    for family_id in title_dict:
+        new_elt = {'family_id': family_id, 'title': title_dict[family_id]}
+        res.append(new_elt)
+
+    df_title = pd.DataFrame(res)
+
+    abstract_dict = {}
+    for row in fam.itertuples():
+        family_id = row.docdb_family_id
+        if family_id not in abstract_dict:
+            abstract_dict[family_id] = []
+        elt = {"fr": row.abstract_fr, "en": row.abstract_en, "default": row.abstract_default}
+        if elt not in abstract_dict[family_id]:
+            abstract_dict[family_id].append(elt)
+    res = []
+    for family_id in abstract_dict:
+        new_elt = {'family_id': family_id, 'summary': abstract_dict[family_id]}
+        res.append(new_elt)
+    df_abstract = pd.DataFrame(res)
+    role_dict = {}
+    for row in role.itertuples():
+        family_id = row.key_appln_nr_person
+        if family_id not in role_dict:
+            role_dict[family_id] = []
+        elt = {"role": row.role, "description": row.role}
+        if elt not in role_dict[family_id]:
+            role_dict[family_id].append(elt)
+    res = []
+    for family_id in role_dict:
+        new_elt = {'family_id': family_id, 'rolePatent': role_dict[family_id]}
+        res.append(new_elt)
+    df_role = pd.DataFrame(res)
+    part2 = pd.merge(part, df_role, left_on="key_appln_nr_person", right_on="family_id", how="left").drop(
+        columns="family_id")
+    affiliation_dict = {}
+    for row in part2.itertuples():
+        family_id = row.key_appln_nr_person
+        if family_id not in affiliation_dict:
+            affiliation_dict[family_id] = []
+        elt = row.siren
+        if elt and elt not in affiliation_dict[family_id]:
+            affiliation_dict[family_id].append(elt)
+    res = []
+    for family_id in affiliation_dict:
+        new_elt = {'family_id': family_id, 'affiliations': affiliation_dict[family_id]}
+        res.append(new_elt)
+    df_affiliation = pd.DataFrame(res)
+    part3 = pd.merge(part2, df_affiliation, left_on="key_appln_nr_person", right_on="family_id", how="left").drop(
+        columns="family_id")
+    authors_dict = {}
+    for row in part3.itertuples():
+        family_id = row.docdb_family_id
+        if family_id not in authors_dict:
+            authors_dict[family_id] = []
+        elt = {"typeParticipant": row.type, "fullName": row.name_corrected, "country": row.country_corrected,
+               "rolePatent": row.rolePatent, "affiliations": row.affiliations}
+        if elt and elt not in authors_dict[family_id]:
+            authors_dict[family_id].append(elt)
+    res = []
+    for family_id in authors_dict:
+        new_elt = {'family_id': family_id, 'authors': authors_dict[family_id]}
+        res.append(new_elt)
+    df_authors = pd.DataFrame(res)
+    fam_authors = jointure(fam, df_authors)
+    fam_titles = jointure(fam_authors, df_title)
+    fam_sum = jointure(fam_titles, df_abstract)
+    fam_domains = jointure(fam_sum, df_fam)
+    fam_pat = jointure(fam_domains, df_pat)
+    affiliation_dict2 = {}
+    for row in part2.itertuples():
+        family_id = row.docdb_family_id
+        if family_id not in affiliation_dict2:
+            affiliation_dict2[family_id] = []
+        elt = row.siren
+        if elt and elt not in affiliation_dict2[family_id]:
+            affiliation_dict2[family_id].append(elt)
+    res = []
+    for family_id in affiliation_dict2:
+        new_elt = {'family_id': family_id, 'affiliations': affiliation_dict2[family_id]}
+        res.append(new_elt)
+    df_affiliation2 = pd.DataFrame(res)
+    fam_affiliations = jointure(fam_pat, df_affiliation2)
+    fam_final = fam_affiliations.drop(
+        columns=["design_patents_count", "patents_count", "utility_models_count", "is_granted", "title_en", "title_fr",
+                 "title_default_language", "title_default", "abstract_en", "abstract_fr", "abstract_default_language",
+                 "abstract_default"])
+    fam_final["year"] = fam_final["earliest_application_date"].apply(get_year)
+    fam_final["productionType"] = "patent"
+    fam_final["inventionKind"] = "brevet"
+    fam_final['isOa'] = False
+    fam_final = fam_final.rename(
+        columns={"docdb_family_id": "id", "inpadoc_family_id": "inpadocFamily",
+                 "earliest_publn_date": "publicationDate",
+                 "earliest_application_date": "submissionDate", "is_oeb": "isOeb",
+                 "is_international": "isInternational",
+                 "date_first_granted": "grantedDate"})
+    for str_field in ["id", "inpadocFamily"]:
+        fam_final[str_field] = fam_final[str_field].apply(lambda x: str(x))
+    fam_final.to_json("fam_final_json_env.jsonl", orient="records", lines=True)
+    swift.upload_object('patstat', 'fam_final_json_env.jsonl')
