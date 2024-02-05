1687d7b9b6ba55d58ac2741be1ed8d2fff595d74
juliaGrandhay <julia.grandhay@gmail.com>
2024-02-05 11:03:36 +0100

    adaptation multi csv

--- patstat/loading_mongo.py
+++ patstat/loading_mongo.py
@@ -1,9 +1,11 @@
 import os
 from pymongo import MongoClient
-from patstat import csv_files_querying as cfq
 import logging
 import sys
 from patstat import dtypes_patstat_declaration as types
+import pandas as pd
+import glob
+from datetime import datetime as dt
 
 DATA_PATH = os.getenv("MOUNTED_VOLUME_TEST")
 
@@ -33,8 +35,8 @@ tls215.create_index(
 DICT = {
     "tls211": {'sep': ',', 'chunksize': 1000000, 'engine': "python", "dtype": types.tls211_types},
     "tls212": {'sep': ',', 'chunksize': 1000000, 'engine': "python", "dtype": types.tls212_types},
-    "tls214":  {'sep': ',', 'chunksize': 1000000, 'engine': "python", "dtype": types.tls214_types},
-    "tls215":  {'sep': ',', 'chunksize': 1000000, 'engine': "python", "dtype": types.tls215_types}
+    "tls214": {'sep': ',', 'chunksize': 1000000, 'engine': "python", "dtype": types.tls214_types},
+    "tls215": {'sep': ',', 'chunksize': 1000000, 'engine': "python", "dtype": types.tls215_types}
 }
 
 
@@ -58,7 +60,26 @@ def get_logger(name):
     return loggers[name]
 
 
+def multi_csv_files_querying(files_directory: str, chunk_query, dict_param_load_csv: dict) -> pd.DataFrame:
+    files_list = glob.glob(files_directory + "/*.csv")
+    df = list(map(lambda a: csv_file_querying(a, chunk_query, dict_param_load_csv), files_list))
+
+    return print("Results tables concatenation")
+
+
+def csv_file_querying(csv_file: str, chunk_query, dict_param_load_csv: dict) -> pd.DataFrame:
+    df_chunk = pd.read_csv(csv_file, sep=dict_param_load_csv.get('sep'), chunksize=dict_param_load_csv.get('chunksize'),
+                           usecols=dict_param_load_csv.get('usecols'), dtype=dict_param_load_csv.get('dtype'))
+    print(f"query beginning {csv_file}: ", dt.now())
+    chunk_result_list = list(map(lambda chunk: chunk_query(chunk), df_chunk))
+    print(f"end of query {csv_file} at : ", dt.now())
+    dataframe_result = pd.concat(chunk_result_list)
+
+    return dataframe_result
+
+
 def chunk_function_211(chunk):
+    chunk = chunk.loc[chunk["pat_publn_id"] > 0]
     js = chunk.to_dict(orient='records')
     x = tls211.insert_many(js)
 
@@ -87,7 +108,9 @@ def chunk_function_215(chunk):
 
 
 def incremental_loading(folder: str, chunk_function, ttls):
-    patent_appln_chunk = cfq.multi_csv_files_querying(folder, chunk_function, DICT.get(ttls))
+    patent_appln_chunk = multi_csv_files_querying(folder, chunk_function, DICT.get(ttls))
+
+    return print(len(patent_appln_chunk))
 
 
 def load_data():
