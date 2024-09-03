a4ce053025cd8ae959c22969fd0ceb5cc24a456f
juliaGrandhay <julia.grandhay@gmail.com>
2024-09-03 08:47:03 +0200

    add files object storage to data folder

--- patstat/p08.py
+++ patstat/p08.py
@@ -9,6 +9,13 @@ from utils import swift
 DATA_PATH = os.getenv('MOUNTED_VOLUME_TEST')
 
 def fun1():
-    swift.download_object('patstat', 'part_p08.csv', f'{DATA_PATH}part_p08.csv')
-    swift.download_object('patstat', 'part_p05.csv', f'{DATA_PATH}part_p05.csv')
-    swift.download_object('patstat', 'part_init_p05.csv', f'{DATA_PATH}part_init_p05.csv')
+    swift.download_object('patstat', 'part_init_p05.csv', f'{DATA_PATH}part_init_p05_bckup.csv')
+    swift.download_object('patstat', 'part_p05.csv', f'{DATA_PATH}part_p05_bckup.csv')
+    swift.download_object('patstat', 'part_p08.csv', f'{DATA_PATH}part_p08_bckup.csv')
+    swift.download_object('patstat', 'part_init_p05_corrected.csv',
+                          f'{DATA_PATH}part_init_p05.csv')
+    swift.download_object('patstat', 'part_init_p05_corrected.csv',
+                          f'{DATA_PATH}part_init_p05_corrected.csv')
+    swift.download_object('patstat', 'part_p05_corrected.csv', f'{DATA_PATH}part_p05.csv')
+    swift.download_object('patstat', 'part_p08_corrected.csv', f'{DATA_PATH}part_p08.csv')
+    swift.download_object('patstat', 'model_xgb.json', f'{DATA_PATH}model_xgb.json')
