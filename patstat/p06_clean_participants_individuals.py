cdde07423c3979d170149178d2dad49f306eea53
juliaGrandhay <julia.grandhay@gmail.com>
2024-09-05 14:45:03 +0200

    uncomment functions

--- patstat/p06_clean_participants_individuals.py
+++ patstat/p06_clean_participants_individuals.py
@@ -305,9 +305,9 @@ def get_clean_ind():
 
     # sex_table = get_sex_from_name_set(df_get_sex)
 
-    # sub_get_sex = subset_df(df_get_sex)
+    sub_get_sex = subset_df(df_get_sex)
 
-    # sex_table = res_futures(sub_get_sex, get_sex_from_name_set)
+    sex_table = res_futures(sub_get_sex, get_sex_from_name_set)
 
     sex_table.to_csv('sex_table.csv', sep='|', index=False)
 
