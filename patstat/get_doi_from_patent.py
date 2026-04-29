c56b6a55ea3f821e410bd345077b1843ee822643
juliaGrandhay <julia.grandhay@gmail.com>
2026-04-29 16:32:03 +0200

    add cleaning steps

--- patstat/get_doi_from_patent.py
+++ patstat/get_doi_from_patent.py
@@ -49,6 +49,8 @@ def get_patents_common_doi():
     tls214["doi_clean"] = tls214["doi_clean"].str.replace(r"\*\s?", "", regex=True, case=False)
     tls214["doi_clean"] = tls214["doi_clean"].str.replace(r"HTTPS?\:\/\/DX\.DOI\.ORG\/", "", regex=True,
                                                           case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("dx.doi.org/", "", regex=False,
+                                                          case=False)
     tls214["doi_clean"] = tls214["doi_clean"].str.replace(r"https?\:\/\/doi\.org\/", "", regex=True,
                                                           case=False)
     tls214["doi_clean"] = tls214["doi_clean"].str.replace(r"\:\s?", "", regex=True,
@@ -67,6 +69,8 @@ def get_patents_common_doi():
                                                           "", regex=True, case=False)
     tls214["doi_clean"] = tls214["doi_clean"].str.replace(r"https?\/\/\:rd\.springer\.com\/article\/",
                                                           "", regex=True, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("https://rd.springer.com/article/",
+                                                          "", regex=False, case=False)
     tls214["doi_clean"] = tls214["doi_clean"].str.replace(r"\,availableonlineathttp\/\/www\.idealibrary\.comon",
                                                           "", regex=True, case=False)
     tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.3390/molecules 171214205",
@@ -115,6 +119,77 @@ def get_patents_common_doi():
     tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1016/j.ajpath. 2014.11.02 9",
                                                           "10.1016/j.ajpath.2014.11.029",
                                                           regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1 0L 6/ J.SOLMAT.2017 .06.023",
+                                                          "10.1016/j.solmat.2017.06.023",
+                                                          regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace(
+        "10.1002/(SICI)1521-396X(199911)1761<297AID-PSSA297>3.0.CO; 2-Z",
+        "10.1002/(SICI)1521-396X(199911)176:1<297::AID-PSSA297>3.0.CO;2-Z",
+        regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1002/ANIE.201608520 10.1002/ANIE.201608520",
+                                                          "10.1002/anie.201608520",
+                                                          regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace(
+        "10.1016/0022-328X(95)05701-P 10.1016/0022-328X(95)05701-P",
+        "10.1016/0022-328X(95)05701-P", regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1016/J.OMTN.2018.09.006 PUBLISHER",
+                                                          "10.1016/J.OMTN.2018.09.006", regex=False,
+                                                          case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace(
+        "10.1016/J.TETLET.2003.11.131 10.1016/J.TETLET.2003.11.131",
+        "10.1016/j.tetlet.2003.11.131", regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace(
+        "10.1016/S0031-9422(00)00274-0 10.1016/S0031-9422(00)00274-0",
+        "10.1016/S0031-9422(00)00274-0", regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace(
+        "10.1016/S0040-4039(03)01075-X 10.1016/S0040-4039(03)01075-X",
+        "10.1016/S0040-4039(03)01075-X", regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1016/S1473-3099(14)71047-3 pubmed25435054",
+                                                          "10.1016/S1473-3099(14)71047-3",
+                                                          regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1016/j.asoc. 2018.05.01 8",
+                                                          "10.1016/j.asoc.2018.05.018", regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1016/j.cyto.2011.07.396 IRN - ISSN 1",
+                                                          "10.1016/j.cyto.2011.07.396", regex=False,
+                                                          case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1016/j.jaccas. 2020.04.05 3",
+                                                          "10.1016/j.jaccas.2020.04.053", regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1080/10575630008043769 10.1080/10575630008043769",
+                                                          "10.1080/10575630008043769",
+                                                          regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1093/JXB/ERP061 10.1093/JXB/ERP061",
+                                                          "10.1093/JXB/ERP061", regex=False,
+                                                          case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1111/ics. 12512",
+                                                          "10.1111/ics.12512", regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1134/S1054660X10030060 Î",
+                                                          "10.1134/S1054660X10030060", regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1371/journal.pone.0033490 pubmed22432029",
+                                                          "10.1371/journal.pone.0033490",
+                                                          regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1517/13543776.2012.733000 CR",
+                                                          "10.1517/13543776.2012.733000", regex=False,
+                                                          case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.4271/ 2009-01-11 43",
+                                                          "10.4271/2009-01-1143", regex=False, case=False)
+    tls214["doi_clean"] = tls214.loc[tls214[
+                                         "npl_biblio"] == "B. RADERBAUER, A. WYSS: \'Tunnel excavation material as rescource for underground power plantes and concrete dam constructions\', GEOMECHANICS AND TUNNELLING, vol. 7, no. 5, 1 October 2014 (2014-10-01), Berlin, pages 451 - 460, XP002805733, ISSN: 1865-7362, Retrieved from the Internet <URL:https://onlinelibrary.wiley.com/doi/10.1002/geot.201400040> [retrieved on 20220218], DOI: 10.1002", "doi_clean"].str.replace(
+        "10.1002",
+        "10.1002/geot.201400040",
+        regex=False, case=False)
+
+    tls214["doi_clean"] = tls214.loc[tls214[
+                                         "npl_biblio"] == "KARAJIC ET AL.: \'Bottom-up Generation of Miniaturized Coaxial Double Electrodes with Tunable Porosity\', ADVANCED MATERIALS INTERFACES, vol. 2, no. 12, 6 July 2015 (2015-07-06), pages 1 - 5, XP002751090, DOI: 10.1002", "doi_clean"].str.replace(
+        "10.1002",
+        "10.1002/admi.201500192",
+        regex=False, case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1.1.14.6782", "10.1109/TNSRE.2002.1021581", regex=False,
+                                                          case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("10.1.1.75.1021", "10.5281/zenodo.40463", regex=False,
+                                                          case=False)
+    tls214["doi_clean"] = tls214["doi_clean"].str.lower()
+    tls214["doi_clean"] = tls214["doi_clean"].str.replace("//",
+                                                          "/", regex=False, case=False)
 
     tls214["doi"] = "https://doi.org/" + tls214["doi_clean"]
 
