aabb09a0e8f2d47b978926e8dc46cd7f3242f29f
juliaGrandhay <julia.grandhay@gmail.com>
2026-05-04 16:51:17 +0200

    requests openalex and treatment json

--- patstat/get_publi_from_oa.py
+++ patstat/get_publi_from_oa.py
@@ -7,73 +7,118 @@ from utils import swift
 import requests
 from retry import retry
 from collections import Counter
+from pathlib import Path
+import time
 
 DATA_PATH = os.getenv('MOUNTED_VOLUME_TEST')
+CACHE_FILE = "/data/openalex_cache.json"
 PAYSAGE = os.getenv("PAYSAGE_API_DUMP")
+OPENALEX_API_KEY = os.getenv("OPENALEX_API_KEY")
+openalex_cache = {}
 
 logger = get_logger(__name__)
 
-@retry(tries=3, delay=5, backoff=5)
-def request_oa(doi: str) -> dict:
-    url_oa = f" https://api.openalex.org/works?filter=doi:{doi}"
-    res_oa = requests.get(url_oa)
-    if res_oa.status_code == 200:
-        res = res_oa.json()
-        for i in range(len(res['results'])):
-            for l in range(len(res["results"][i]["authorships"])):
-                dic = res["results"][i]["authorships"][l]["author"]
-                print("doi")
-                dic["doi"] = res["results"][i]["doi"]
-                print("display_name_title")
-                dic["display_name_title"] = res["results"][i]["display_name"]
-                print("title")
-                dic["title"] = res["results"][i]["title"]
-                dic["id_pub"] = res["results"][i]["id"]
-                dic["language"] = res["results"][i]["language"]
-                dic["type"] = res["results"][i]["type"]
-                dic["is_retracted"] = res["results"][i]["is_retracted"]
-                dic["publication_year"] = res["results"][i]["publication_year"]
-                dic["publication_date"] = res["results"][i]["publication_date"]
-                dic["is_oa"] = res["results"][i]["open_access"]["is_oa"]
-                dic["volume"] = res["results"][i]["biblio"]["volume"]
-                dic["issue"] = res["results"][i]["biblio"]["issue"]
-                dic["first_page"] = res["results"][i]["biblio"]["first_page"]
-                dic["last_page"] = res["results"][i]["biblio"]["last_page"]
-                dic["pdf_url"] = res["results"][i]["primary_location"]["pdf_url"]
-                dic["raw_type"] = res["results"][i]["primary_location"]["raw_type"]
-                dic["is_accepted"] = res["results"][i]["primary_location"]["is_accepted"]
-                dic["is_published"] = res["results"][i]["primary_location"]["is_published"]
-                print("raw_source_name")
-                dic["raw_source_name"] = res["results"][i]["primary_location"]["raw_source_name"]
-                if "source" in res["results"][i]["primary_location"]:
-                    if isinstance(res["results"][i]["primary_location"]["source"], dict):
-                        print("id_source")
-                        dic["id_source"] = res["results"][i]["primary_location"]["source"]["id"]
-                        print("issn_l")
-                        dic["issn_l"] = res["results"][i]["primary_location"]["source"]["issn_l"]
-                        print("host_organization_name")
-                        dic["host_organization_name"] = res["results"][i]["primary_location"]["source"][
-                            "host_organization_name"]
-                else:
-                    print("id_source None")
-                    dic["id_source"] = None
-                    print("issn_l None")
-                    dic["issn_l"] = None
-                    print("id_source None")
-                    dic["host_organization_name"] = None
-                keys_ins = []
-                if len(res["results"][i]["authorships"][l]["institutions"]) > 0:
-                    for li in range(len(res["results"][i]["authorships"][l]["institutions"])):
-                        for key in res["results"][i]["authorships"][l]["institutions"][li].keys():
-                            if key != "lineage":
-                                key2 = key + "_ins"
-                                keys_ins.append(key2)
-                                dic[key2] = res["results"][i]["authorships"][l]["institutions"][li][key]
-                for item in ["author", "institutions", "raw_affiliation_strings"]:
-                    if item in dic.keys():
-                        del dic[item]
 
-    return dic
+@retry(tries=3, delay=5, backoff=5)
+def fetch_openalex(dois: list, reset_cache: False):
+    cache = {}
+    dois = list(set(dois))
+    if not reset_cache and Path(CACHE_FILE).exists():
+        with open(CACHE_FILE) as f:
+            cache = json.load(f)
+
+    missing = [doi for doi in dois if doi not in cache]
+    print(missing)
+
+    for i in range(0, len(missing), 50):
+        batch = missing[i:i + 50]
+        filter_str = "|".join(batch)
+
+        for attempt in range(3):
+            try:
+                print(f"fetching 50 DOIs")
+                r = requests.get(
+                    "https://api.openalex.org/works",
+                    params={"filter": f"doi:{filter_str}", "per-page": 50, "api_key": OPENALEX_API_KEY},
+                    timeout=30
+                )
+                r.raise_for_status()
+                for work in r.json().get("results", []):
+                    doi = work.get("doi", "").replace("https://doi.org/", "")
+                    cache[doi] = work
+                break
+            except Exception as e:
+                print(f"Attempt {attempt + 1}/3 failed: {e}")
+                time.sleep(2 ** attempt)  # backoff exponentiel : 1s, 2s, 4s
+
+        time.sleep(0.1)
+
+    with open(CACHE_FILE, "w") as f:
+        json.dump(cache, f)
+
+    authors = []
+    for doi in dois:
+        if doi in cache:
+            res = cache[doi]
+        for l in range(len(res["authorships"])):
+            dic = res["authorships"][l]["author"]
+            print("doi")
+            dic["doi"] = res["doi"]
+            print("display_name_title")
+            dic["display_name_title"] = res["display_name"]
+            print("title")
+            dic["title"] = res["title"]
+            dic["id_pub"] = res["id"]
+            dic["language"] = res["language"]
+            dic["type"] = res["type"]
+            dic["is_retracted"] = res["is_retracted"]
+            dic["publication_year"] = res["publication_year"]
+            dic["publication_date"] = res["publication_date"]
+            dic["is_oa"] = res["open_access"]["is_oa"]
+            dic["volume"] = res["biblio"]["volume"]
+            dic["issue"] = res["biblio"]["issue"]
+            dic["first_page"] = res["biblio"]["first_page"]
+            dic["last_page"] = res["biblio"]["last_page"]
+            dic["pdf_url"] = res["primary_location"]["pdf_url"]
+            dic["raw_type"] = res["primary_location"]["raw_type"]
+            dic["is_accepted"] = res["primary_location"]["is_accepted"]
+            dic["is_published"] = res["primary_location"]["is_published"]
+            print("raw_source_name")
+            dic["raw_source_name"] = res["primary_location"]["raw_source_name"]
+            if "source" in res["primary_location"]:
+                if isinstance(res["primary_location"]["source"], dict):
+                    print("id_source")
+                    dic["id_source"] = res["primary_location"]["source"]["id"]
+                    print("issn_l")
+                    dic["issn_l"] = res["primary_location"]["source"]["issn_l"]
+                    print("host_organization_name")
+                    dic["host_organization_name"] = res["primary_location"]["source"][
+                        "host_organization_name"]
+            else:
+                print("id_source None")
+                dic["id_source"] = None
+                print("issn_l None")
+                dic["issn_l"] = None
+                print("id_source None")
+                dic["host_organization_name"] = None
+            keys_ins = []
+            if len(res["authorships"][l]["institutions"]) > 0:
+                for li in range(len(res["authorships"][l]["institutions"])):
+                    for key in res["authorships"][l]["institutions"][li].keys():
+                        if key != "lineage":
+                            key2 = key + "_ins"
+                            keys_ins.append(key2)
+                            dic[key2] = res["authorships"][l]["institutions"][li][key]
+            for item in ["author", "institutions", "raw_affiliation_strings"]:
+                if item in dic.keys():
+                    del dic[item]
+            authors.append(dic)
+
+    df = pd.DataFrame(data=authors)
+    df = df.drop_duplicates().reset_index(drop=True)
+    df["ror"] = df["ror_ins"].str.replace("https://ror.org/", "", regex=False)
+
+    return df
 
 
 @retry(tries=3, delay=5, backoff=5)
@@ -176,17 +221,9 @@ def get_info_publi():
     publi = pd.read_csv("publi_brevets.csv", sep="|", encoding="utf-8", engine="python",
                         dtype=types.tls214_types)
 
-    dois_uniques = list(publi["doi"].unique())
+    dois_uniques = list(publi["doi_clean"].unique())
 
-    authors = []
-    for doi in dois_uniques:
-        dic_authors = request_oa(doi)
-        authors.append(dic_authors)
-
-    df_authors = pd.DataFrame(authors)
-
-    df_authors = df_authors.drop_duplicates().reset_index(drop=True)
-    df_authors["ror"] = df_authors["ror_ins"].str.replace("https://ror.org/", "", regex=False)
+    df_authors = fetch_openalex(dois_uniques, False)
 
     df_authors2 = pd.merge(publi[["key_appln_nr", "appln_id", "docdb_family_id", "pat_publn_id", "doi_clean", "doi"]],
                      df_authors, on="doi", how="inner")
