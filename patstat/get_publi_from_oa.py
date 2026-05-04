61a12dbf2f70fca9cd8242ba5c13b516f27cb347
juliaGrandhay <julia.grandhay@gmail.com>
2026-05-04 11:16:20 +0200

    corrections

--- patstat/get_publi_from_oa.py
+++ patstat/get_publi_from_oa.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import os
+import json
 from patstat import dtypes_patstat_declaration as types
 from application.server.main.logger import get_logger
 from utils import swift
 import requests
 from retry import retry
+from collections import Counter
 
 DATA_PATH = os.getenv('MOUNTED_VOLUME_TEST')
+PAYSAGE = os.getenv("PAYSAGE_API_DUMP")
 
 logger = get_logger(__name__)
 
@@ -73,6 +76,99 @@ def request_oa(doi: str) -> dict:
     return dic
 
 
+@retry(tries=3, delay=5, backoff=5)
+def ids_paysage():
+    rres_data = []
+
+    res = requests.get(
+        "https://api.paysage.dataesr.ovh/dump/structures",
+        headers={"Content-Type": "application/json", "X-API-KEY": PAYSAGE})
+
+    with open("dump_paysage.txt", "w") as f:
+        f.write(res.text)
+
+    with open("dump_paysage.txt", "r") as f:
+        dump_paysage = f.readlines()
+
+    dump_paysage = [json.loads(item) for item in dump_paysage]
+
+    for etab in dump_paysage:
+        dict_res = {}
+        current = etab.get("currentName")
+        name = current.get("usualName")
+        res_data = etab.get("category")
+        dict_res["id_paysage"] = etab.get("id")
+        pid = etab.get("id")
+        dict_res["usualName"] = name
+        if len(res_data) > 0:
+            identifiers = etab.get("identifiers")
+            types = [item.get("type") for item in identifiers]
+            decompte = Counter(types)
+            if "siret" in types:
+                if decompte["siret"] > 1:
+                    for item in identifiers:
+                        if pid == "9ZF2M":
+                            siret = "19341087500010"
+                            siren = "193410875"
+                            dict_res["siren"] = siren
+                            dict_res["siret"] = siret
+                        elif item.get("type") == "siret":
+                            if item.get("active") == True:
+                                siret = item.get("value")
+                                siren = siret[0:9]
+                                dict_res["siren"] = siren
+                                dict_res["siret"] = siret
+                elif decompte["siret"] == 1:
+                    for item in identifiers:
+                        if item.get("type") == "siret":
+                            siret = item.get("value")
+                            siren = siret[0:9]
+                            dict_res["siren"] = siren
+                            dict_res["siret"] = siret
+            else:
+                dict_res["siren"] = ""
+                dict_res["siret"] = ""
+            if "grid" in types:
+                if decompte["grid"] > 1:
+                    for item in identifiers:
+                        if item.get("type") == "grid":
+                            if item.get("active") == True:
+                                grid = item.get("value")
+                                dict_res["grid"] = grid
+                elif decompte["grid"] == 1:
+                    for item in identifiers:
+                        if item.get("type") == "grid":
+                            grid = item.get("value")
+                            dict_res["grid"] = grid
+            else:
+                dict_res["grid"] = ""
+            if "ror" in types:
+                for item in identifiers:
+                    if item.get("type") == "ror":
+                        ror = item.get("value")
+                        dict_res["ror"] = ror
+            else:
+                dict_res["ror"] = ""
+            if "idref" in types:
+                for item in identifiers:
+                    if item.get("type") == "idref":
+                        idref = item.get("value")
+                        idref = "idref" + idref
+                        dict_res["idref_ins"] = idref
+            else:
+                dict_res["idref_ins"] = ""
+        rres_data.append(dict_res)
+
+    df_res = pd.DataFrame(rres_data)
+    df_res = df_res.drop_duplicates().reset_index(drop=True)
+    df_res = df_res.loc[df_res["ror"].notna()]
+    df_res = df_res.loc[df_res["ror"] != ""]
+    compte_id_paysage = df_res[["ror", "id_paysage"]].groupby("ror").nunique(dropna=False).reset_index()
+    df_res = df_res.loc[df_res["ror"].isin(compte_id_paysage.loc[compte_id_paysage["id_paysage"] == 1, "ror"])]
+
+    return df_res
+
+
 def get_info_publi():
     # set working directory
     os.chdir(DATA_PATH)
@@ -88,17 +184,51 @@ def get_info_publi():
         authors.append(dic_authors)
 
     df_authors = pd.DataFrame(authors)
-    df_publi = df_authors[["doi", "display_name_title", "title", "id_pub", "language", "type", "is_retracted",
-                           "publication_year", "publication_date", "is_oa", "volume", "issue", "first_page",
-                           "last_page", "pdf_url", "raw_type", "is_accepted", "is_published", "raw_source_name",
-                           "id_source", "issn_l", "host_organization_name"]].drop_duplicates().reset_index(drop=True)
-    df_publi.to_csv("df_publi.csv", sep="|", encoding="utf-8", index=False)
-    swift.upload_object('patstat', 'df_publi.csv')
 
-    df_authors2 = pd.merge(publi, df_authors, on="doi", how="left")
+    df_authors = df_authors.drop_duplicates().reset_index(drop=True)
+    df_authors["ror"] = df_authors["ror_ins"].str.replace("https://ror.org/", "", regex=False)
+
+    df_authors2 = pd.merge(publi[["key_appln_nr", "appln_id", "docdb_family_id", "pat_publn_id", "doi_clean", "doi"]],
+                     df_authors, on="doi", how="inner")
     df_authors2 = df_authors2.drop_duplicates().reset_index(drop=True)
-    df_authors2.to_csv("df_authors.csv", sep="|", encoding="utf-8", index=False)
-    swift.upload_object('patstat', 'df_authors.csv')
+    df_authors2["publication_year"] = df_authors2["publication_year"].astype(str)
+    data_missing = publi.loc[~publi["doi"].isin(df_authors2["doi"])]
+    data_missing = data_missing.drop_duplicates().reset_index(drop=True)
+
+    data_missing.loc[data_missing["npl_type"] == "b", "type"] = "book-citation"
+    data_missing.loc[data_missing["npl_type"] == "s", "type"] = "serial/journal/periodical citation"
+    data_missing.loc[data_missing["npl_type"] == "w", "type"] = "world wide web/internet search citation"
+    data_missing.loc[data_missing["npl_publn_date"] == "20880630", "npl_publn_date"] = "20080601"
+    data_missing.loc[data_missing["npl_publn_date"] == "10160618", "npl_publn_date"] = "20160624"
+    data_missing.loc[data_missing["npl_publn_date"].notna(), "publication_year"] = data_missing.loc[
+        data_missing["npl_publn_date"].notna(), "npl_publn_date"].apply(lambda a: a[0:4])
+    data_missing.loc[data_missing["npl_publn_date"].notna(), "len_date"] = data_missing.loc[
+        data_missing["npl_publn_date"].notna(), "npl_publn_date"].apply(lambda a: len(a))
+    data_missing.loc[data_missing["len_date"] == 8, "npl_publn_date"] = data_missing.loc[
+        data_missing["len_date"] == 8, "npl_publn_date"].apply(
+        lambda a: a[0:4] if "0000" in a else a[0:4] + "-" + a[4:6] + "-" + a[6:8])
+    data_missing.loc[data_missing["len_date"] == 6, "npl_publn_date"] = data_missing.loc[
+        data_missing["len_date"] == 6, "npl_publn_date"].apply(lambda a: a[0:4] + "-" + a[4:6])
+
+    data_missing = data_missing.rename(columns={"npl_title1": "display_name_title", "npl_title2": "raw_source_name",
+                                                "npl_author": "display_name", "online_availability": "pdf_url",
+                                                "npl_issn": "issn_l", "npl_publisher": "host_organization_name",
+                                                "npl_publn_date": "publication_date", "npl_issue": "issue",
+                                                "npl_volume": "volume", "npl_page_first": "first_page",
+                                                "npl_page_last": "last_page"})
+    data_missing = data_missing.drop(columns=["npl_publn_id", "xp_nr", "npl_type", "npl_biblio", "npl_editor",
+                                              "npl_publn_end_date", "npl_abstract_nr", "npl_doi", "npl_isbn",
+                                              "online_classification", "online_search_date", "len_date"])
+
+    data_missing = data_missing.drop_duplicates().reset_index(drop=True)
+
+    df_paysage = ids_paysage()
+
+    oa = pd.concat([df_authors2, data_missing], ignore_index=True)
+    oa2 = pd.merge(oa, df_paysage, on="ror", how="left")
+
+    oa2.to_csv("publi_oa.csv", sep="|", encoding="utf-8", index=False)
+    swift.upload_object('patstat', 'publi_oa.csv')
 
 
 
