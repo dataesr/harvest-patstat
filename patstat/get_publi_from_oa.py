7c5a67791eee5847bb02c3edd96d77669fedd0e1
juliaGrandhay <julia.grandhay@gmail.com>
2026-06-05 17:27:48 +0200

    use package scientific_text_cleaner

--- patstat/get_publi_from_oa.py
+++ patstat/get_publi_from_oa.py
@@ -1,10 +1,8 @@
 import gzip
 import json
 import os
-import re
 import shutil
 import time
-import html
 from collections import Counter
 from pathlib import Path
 from urllib.parse import unquote
@@ -15,6 +13,7 @@ import requests
 from retry import retry
 
 from patstat import dtypes_patstat_declaration as types
+from scientific_text_cleaner import clean_text
 from utils import swift
 
 DATA_PATH = os.getenv('MOUNTED_VOLUME_TEST')
@@ -22,113 +21,6 @@ CACHE_FILE = "/data/openalex_cache.json"
 PAYSAGE = os.getenv("PAYSAGE_API_DUMP")
 OPENALEX_API_KEY = os.getenv("OPENALEX_API_KEY")
 openalex_cache = {}
-
-LATEX_SYMBOLS = {r"\alpha": "α", r"\beta": "β", r"\gamma": "γ", r"\delta": "δ", r"\epsilon": "ε", r"\theta": "θ",
-                 r"\lambda": "λ", r"\mu": "μ", r"\pi": "π", r"\sigma": "σ", r"\phi": "φ", r"\omega": "ω",
-                 r"\Delta": "Δ", r"\Sigma": "Σ", r"\int": "∫", r"\sum": "Σ", r"\sqrt": "√", r"\pm": "±"}
-
-SUPERSCRIPTS = {"0": "⁰", "1": "¹", "2": "²", "3": "³", "4": "⁴", "5": "⁵", "6": "⁶", "7": "⁷", "8": "⁸", "9": "⁹",
-                "+": "⁺", "-": "⁻", "=": "⁼", "(": "⁽", ")": "⁾", "n": "ⁿ", "i": "ⁱ"}
-
-SUBSCRIPTS = {"0": "₀", "1": "₁", "2": "₂", "3": "₃", "4": "₄", "5": "₅", "6": "₆", "7": "₇", "8": "₈", "9": "₉",
-              "+": "₊", "-": "₋", "=": "₌", "(": "₍", ")": "₎", "i": "ᵢ", "j": "ⱼ", "k": "ₖ", "n": "ₙ"}
-
-
-def parse_latex(expr):
-    # Recursive LaTeX parser to clean strings in publications variables
-    if not isinstance(expr, str):
-        return expr
-
-    expr = expr.strip()
-
-    # remove $...$
-    expr = re.sub(r"\$(.*?)\$", r"\1", expr)
-
-    # correction SigmaDelta
-    expr = re.sub(r"SigmaDelta", "ΣΔ", expr)
-
-    # symbols
-    for k, v in LATEX_SYMBOLS.items():
-        expr = expr.replace(k, v)
-
-    def replace_frac(text: str) -> str:
-        # fractions
-        pattern = r"\\frac\{([^{}]+)\}\{([^{}]+)\}"
-        while re.search(pattern, text):
-            text = re.sub(pattern, lambda m: f"{parse_latex(m.group(1))}/{parse_latex(m.group(2))}", text)
-
-        return text
-
-    expr = replace_frac(expr)
-
-    # squares
-    expr = re.sub(r"√\{([^{}]+)\}", lambda m: f"√({parse_latex(m.group(1))})", expr)
-
-    def sup_repl(match):
-        # subscript
-        content = parse_latex(match.group(1))
-
-        return "".join(SUPERSCRIPTS.get(c, f"^{c}") for c in content)
-
-    expr = re.sub(r"\^\{([^{}]+)\}", sup_repl, expr)
-    expr = re.sub(r"\^(\w+)", lambda m: "".join(SUPERSCRIPTS.get(c, c) for c in m.group(1)), expr)
-
-    def sub_repl(match):
-        # subscripts
-        content = parse_latex(match.group(1))
-
-        return "".join(SUBSCRIPTS.get(c, f"_{c}") for c in content)
-
-    expr = re.sub(r"_\{([^{}]+)\}", sub_repl, expr)
-    expr = re.sub(r"_(\w+)", lambda m: "".join(SUBSCRIPTS.get(c, c) for c in m.group(1)), expr)
-
-    # remove {}
-    expr = expr.replace("{", "").replace("}", "")
-
-    return expr
-
-
-def replace_tex_blocks(text: str) -> str:
-    # remove TeX blocks
-    def repl(match):
-        latex = match.group(1)
-        parsed = parse_latex(latex)
-
-        return f" {parsed} "  # keep spaces
-
-    return re.sub(r"<tex[^>]*>(.*?)</tex>", repl, text)
-
-
-def clean_text(value):
-    # cleaning function that applies all the other cleaning functions
-    if not isinstance(value, str):
-        return value
-
-    # HTML decode (multi-pass)
-    prev = None
-    while prev != value:
-        prev = value
-        value = html.unescape(value)
-
-    # URL decode
-    value = unquote(value)
-
-    # TeX blocks
-    value = replace_tex_blocks(value)
-
-    # parse LaTeX global
-    value = parse_latex(value)
-
-    # remove invisible characters
-    value = value.replace('\r', ' ')
-    value = value.replace('\n', ' ')
-    value = value.replace('\t', ' ')
-    value = re.sub(r'[\x00-\x1F\x7F]', '', value)
-
-    # remove multiple spaces
-    value = re.sub(r"\s+", " ", value)
-
-    return value.strip()
 
 
 @retry(tries=3, delay=5, backoff=5)
@@ -441,10 +333,14 @@ def get_info_publi():
 
     oa2["year"] = oa2["publication_year"].astype(pd.Int64Dtype())
 
-    columns = ["doi_clean", "id", "orcid", "pdf_url", "id_source", "id_ins", "ror_ins", "display_name",
-                "display_name_title", "language", "type", "volume", "issue", "raw_source_name", "issn_l",
-                "host_organization_name", "display_name_ins", "country_code_ins",
-                "type_ins", "idref_ins"]
+    col_url = ["id", "orcid", "pdf_url", "id_source", "id_ins", "ror_ins"]
+
+    for col in col_url:
+        oa2.loc[oa2[col].notna(), col] = oa2.loc[oa2[col].notna(), col].apply(unquote)
+
+    columns = ["doi_clean", "display_name", "display_name_title", "language", "type", "volume", "issue",
+               "raw_source_name",
+               "issn_l", "host_organization_name", "display_name_ins", "country_code_ins", "type_ins", "idref_ins"]
 
     for col in columns:
         oa2.loc[oa2[col].notna(), col] = oa2.loc[oa2[col].notna(), col].apply(clean_text)
