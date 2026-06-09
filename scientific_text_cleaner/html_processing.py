d5fdd5fe087895a091b537aa4329cade53fd151a
juliaGrandhay <julia.grandhay@gmail.com>
2026-06-09 11:42:13 +0200

    remove conversion to italics

--- scientific_text_cleaner/html_processing.py
+++ scientific_text_cleaner/html_processing.py
@@ -6,24 +6,24 @@ SUB_MAP = str.maketrans("0123456789+-=()", "₀₁₂₃₄₅₆₇₈₉₊₋
 SUP_MAP = str.maketrans("0123456789+-=()", "⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾")
 
 
-def to_italic(text: str) -> str:
-    res = []
-    for c in text:
-        if 'A' <= c <= 'Z':
-            res.append(chr(0x1D434 + ord(c) - ord('A')))
-        elif 'a' <= c <= 'z':
-            res.append(chr(0x1D44E + ord(c) - ord('a')))
-        else:
-            res.append(c)
-    return "".join(res)
+# def to_italic(text: str) -> str:
+#     res = []
+#     for c in text:
+#         if 'A' <= c <= 'Z':
+#             res.append(chr(0x1D434 + ord(c) - ord('A')))
+#         elif 'a' <= c <= 'z':
+#             res.append(chr(0x1D44E + ord(c) - ord('a')))
+#         else:
+#             res.append(c)
+#     return "".join(res)
 
 
 def parse_html(text: str) -> str:
     soup = BeautifulSoup(text, "html.parser")
 
-    for tag in soup.find_all("i"):
-        tag.replace_with(to_italic(tag.get_text()))
-
+    # for tag in soup.find_all("i"):
+    #     tag.replace_with(to_italic(tag.get_text()))
+    #
     for tag in soup.find_all("sub"):
         tag.replace_with(tag.get_text().translate(SUB_MAP))
 
