fedf15a9e0d34b976f3674c48c47b4f8685ddd48
juliaGrandhay <julia.grandhay@gmail.com>
2026-07-23 17:28:06 +0200

    load tls231 from objectstorage

--- patstat/p08.py
+++ patstat/p08.py
@@ -8,6 +8,7 @@ from utils import swift
 # directory where the files are
 DATA_PATH = os.getenv('MOUNTED_VOLUME_TEST')
 
+
 def fun1():
     # swift.download_object('patstat', 'part_init_p05.csv', f'{DATA_PATH}part_init_p05_bckup.csv')
     # swift.download_object('patstat', 'part_p05.csv', f'{DATA_PATH}part_p05_bckup.csv')
@@ -19,5 +20,9 @@ def fun1():
     # swift.download_object('patstat', 'part_p05_corrected.csv', f'{DATA_PATH}part_p05.csv')
     # swift.download_object('patstat', 'part_p08_corrected.csv', f'{DATA_PATH}part_p08.csv')
     # swift.download_object('patstat', 'model_xgb.json', f'{DATA_PATH}model_xgb.json')
-    swift.download_object('patstat', 'spring2025_fam_final_json.jsonl',
-                          f'{DATA_PATH}spring2025_fam_final_json.jsonl')
+    # swift.download_object('patstat', 'spring2025_fam_final_json.jsonl',
+    #                       f'{DATA_PATH}spring2025_fam_final_json.jsonl')
+
+    for nr in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14"]:
+        swift.download_object('patstat', f'tls231_inpadoc_legal_event_part{nr}.zip',
+                              f'{DATA_PATH}tls231_inpadoc_legal_event_part{nr}.zip')
