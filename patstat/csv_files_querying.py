1787cc0c4fb743c1c4abe47e85113bbcb7a6147b
juliaGrandhay <julia.grandhay@gmail.com>
2026-07-24 10:55:13 +0200

    logger instead of print

--- patstat/csv_files_querying.py
+++ patstat/csv_files_querying.py
@@ -1,6 +1,11 @@
-from datetime import datetime as dt
 import glob
+from datetime import datetime as dt
+
 import pandas as pd
+
+from application.server.main.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 # Chargement des données multi-CSV et requêtes
@@ -8,16 +13,16 @@ def multi_csv_files_querying(files_directory: str, chunk_query, dict_param_load_
     files_list = glob.glob(files_directory + "/*.csv")
     df = list(map(lambda a: csv_file_querying(a, chunk_query, dict_param_load_csv), files_list))
     ending_dataframe = pd.concat(df)
-    print("Results tables concatenation")
+    logger.debug("Results tables concatenation")
     return ending_dataframe
 
 
 def csv_file_querying(csv_file: str, chunk_query, dict_param_load_csv: dict) -> pd.DataFrame:
     df_chunk = pd.read_csv(csv_file, sep=dict_param_load_csv.get('sep'), chunksize=dict_param_load_csv.get('chunksize'),
                            usecols=dict_param_load_csv.get('usecols'), dtype=dict_param_load_csv.get('dtype'))
-    print(f"query beginning {csv_file}: ", dt.now())
+    logger.debug(f"query beginning {csv_file}: ", dt.now())
     chunk_result_list = list(map(lambda chunk: chunk_query(chunk), df_chunk))
-    print(f"end of query {csv_file} at : ", dt.now())
+    logger.debug(f"end of query {csv_file} at : ", dt.now())
     dataframe_result = pd.concat(chunk_result_list)
 
     return dataframe_result
@@ -34,13 +39,14 @@ def filtering(files_directory: str, pat: pd.DataFrame, colfilter: str, dict_para
     :param dict_param_load_csv: dictionary with loading parameters
     :return: df with filtered values
     """
-    print(f"start loading {files_directory}.")
+    logger.debug(f"start loading {files_directory}.")
+
     def lect_patstat_table(chunk):
         query = chunk[chunk[colfilter].isin(pat[colfilter])]
 
         return query
 
     table = multi_csv_files_querying(files_directory, lect_patstat_table, dict_param_load_csv)
-    print(f"End loading {files_directory}.")
+    logger.debug(f"End loading {files_directory}.")
 
     return table
