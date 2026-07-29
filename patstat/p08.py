b5c568f822b2febb2bfd5fe3f19687f8076fe294
juliaGrandhay <julia.grandhay@gmail.com>
2026-07-29 13:34:24 +0200

    download tls files in os and put them in folders

--- patstat/p08.py
+++ patstat/p08.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 
 import os
+import re
+import pandas as pd
+import shutil
 
 from utils import swift
 
@@ -23,6 +26,35 @@ def fun1():
     # swift.download_object('patstat', 'spring2025_fam_final_json.jsonl',
     #                       f'{DATA_PATH}spring2025_fam_final_json.jsonl')
 
-    for nr in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14"]:
-        swift.download_object('patstat', f'tls231_inpadoc_legal_event_part{nr}.zip',
-                              f'{DATA_PATH}tls231_inpadoc_legal_event_part{nr}.zip')
+    # for nr in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14"]:
+    #     swift.download_object('patstat', f'tls231_inpadoc_legal_event_part{nr}.zip',
+    #                           f'{DATA_PATH}tls231_inpadoc_legal_event_part{nr}.zip')
+
+    # lists all the CSV files and rename them (model tls\d{3}_part\d{2}
+
+    list_csv = ["tls201_part01.csv", "tls201_part02.csv", "tls201_part03.csv", "tls202_part01.csv", "tls203_part01.csv",
+           "tls203_part02.csv", "tls203_part03.csv", "tls203_part04.csv", "tls203_part05.csv", "tls203_part06.csv",
+           "tls203_part07.csv", "tls203_part08.csv", "tls203_part09.csv", "tls203_part10.csv", "tls204_part01.csv",
+           "tls206_part01.csv", "tls206_part02.csv", "tls207_part01.csv", "tls209_part01.csv", "tls209_part02.csv",
+           "tls209_part03.csv", "tls211_part01.csv", "tls211_part02.csv", "tls212_part01.csv", "tls212_part02.csv",
+           "tls212_part03.csv", "tls212_part04.csv", "tls214_part01.csv", "tls214_part02.csv", "tls224_part01.csv",
+           "tls224_part02.csv", "tls225_part01.csv", "tls225_part02.csv", "tls902_part01.csv", "tls231_part14.csv",
+           "tls231_part13.csv", "tls231_part12.csv", "tls231_part11.csv", "tls231_part10.csv", "tls231_part09.csv",
+           "tls231_part08.csv", "tls231_part07.csv", "tls231_part06.csv", "tls231_part05.csv", "tls231_part04.csv",
+           "tls231_part03.csv", "tls231_part02.csv", "tls231_part01.csv", "tls229_part01.csv"]
+
+    for csv in list_csv:
+        swift.download_object('patstat', csv, f'{DATA_PATH}{csv}')
+        new_name = csv.split("_")[0] + "_" + csv.split("_")[-1]
+        os.rename(csv, new_name)
+
+    # removes the end of the folders to get table names from PATSTAT Global database
+    table_names = list(map(lambda a: re.sub(r"_.+_?.+?_part\d+.csv", "", a), list_csv))
+
+    # creates a dataframe to connect each file to its folder and table
+    file_names = pd.DataFrame({"subfolders": list_csv, "table_names": table_names})
+    file_names["file_names"] = file_names["subfolders"].apply(lambda a: a.split("_")[0] + "_" + a.split("_")[-1])
+    # moves each file into a folder corresponding to its table in PATSTAT Global database
+    for _, r in file_names.iterrows():
+        os.makedirs(r["table_names"], exist_ok=True)
+        shutil.move(r["file_names"], r["table_names"])
